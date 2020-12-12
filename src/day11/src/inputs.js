b0afebad3c523eae18fe58bd34c88edee8e063cc
Peter Seale <peter@pseale.com>
2020-12-11 21:54:51 -0600

    simulate day 11 Part A (badly)

--- src/day11/src/inputs.js
+++ src/day11/src/inputs.js
@@ -16,28 +16,109 @@ function parse(input) {
     .filter((x) => x);
   const rows = [];
   for (let line of lines) {
-    const row = line.split("").map((x) => (x === "L" ? 1 : 0));
+    const row = line.split("").map((x) => (x === "L" ? SEAT_EMPTY : SQUARE_EMPTY));
     rows.push(row);
   }
   return rows;
 }
 
-const hardcodedRows = [];
-hardcodedRows.push([2, 0, 1, 1, 0, 1, 1, 0, 1, 1]);
-hardcodedRows.push([1, 2, 1, 1, 1, 1, 1, 0, 1, 1]);
-hardcodedRows.push([1, 0, 1, 0, 1, 0, 0, 1, 0, 0]);
-hardcodedRows.push([1, 1, 1, 1, 0, 1, 1, 0, 1, 1]);
-hardcodedRows.push([1, 0, 1, 1, 0, 1, 1, 0, 1, 1]);
-hardcodedRows.push([1, 0, 1, 1, 2, 1, 1, 0, 1, 1]);
-hardcodedRows.push([0, 0, 1, 0, 1, 0, 0, 0, 0, 0]);
-hardcodedRows.push([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
-hardcodedRows.push([1, 0, 1, 1, 1, 1, 1, 1, 0, 1]);
-hardcodedRows.push([1, 0, 1, 1, 1, 1, 1, 0, 1, 1]);
+// assume a & b are arrays of arrays
+function same(a, b) {
+  if (a.length !== b.length) return false;
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i].length !== b[i].length) return false;
+
+    for (let j = 0; j < a[i].length; j++) {
+      if (a[i][j] !== b[i][j]) return false;
+    }
+  }
+
+  return true;
+}
+
+function getNeighboringSquares(rows, row, col) {
+  const offsets = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
+
+  const squares = [];
+
+  for (let offset of offsets) {
+    if (
+      row + offset[0] < rows.length &&
+      row + offset[0] >= 0 &&
+      col + offset[1] < rows[0].length &&
+      col + offset[1] >= 0
+    )
+      squares.push(rows[row + offset[0]][col + offset[1]]);
+  }
+
+  return squares;
+}
+
+const SQUARE_EMPTY = 0;
+const SEAT_EMPTY = 1;
+const SEAT_OCCUPIED = 2;
+
+function isTooCrowded(rows, row, col) {
+  const squares = getNeighboringSquares(rows, row, col);
+
+  return squares.filter((x) => x === SEAT_OCCUPIED).length >= 4;
+}
+
+function hasNoNeighbors(rows, row, col) {
+  const squares = getNeighboringSquares(rows, row, col);
+
+  return squares.every((x) => x === SEAT_EMPTY);
+}
+
+function simulateSquare(rows, row, col) {
+  const square = rows[row][col];
+  if (square === SQUARE_EMPTY) return SQUARE_EMPTY;
+
+  if (square === SEAT_OCCUPIED) {
+    return isTooCrowded(rows, row, col) ? SEAT_EMPTY : SEAT_OCCUPIED;
+  } else if (square === SEAT_EMPTY) {
+    return hasNoNeighbors(rows, row, col) ? SEAT_OCCUPIED : SEAT_EMPTY;
+  } else {
+    throw `invalid square ${col},${row}: ${square}`;
+  }
+}
+
+function simulateNextFrame(rows) {
+  const columns = rows[0].length; // assume non-jagged arrays
+  const r = [];
+  for (let row = 0; row < rows.length; row++) {
+    const rowArray = [];
+    for (let col = 0; col < columns; col++) {
+      rowArray.push(simulateSquare(rows, row, col));
+    }
+    r.push(rowArray);
+  }
+
+  return r;
+}
+
 function simulateFrames(rows) {
   const frames = [];
 
   frames.push(rows);
-  frames.push(hardcodedRows);
+  while (true) {
+    let frame = simulateNextFrame(rows);
+    if (same(frames[frames.length - 1], frame)) {
+      return frames;
+    } else {
+      frames.push(frame);
+    }
+  }
 
   return frames;
 }
