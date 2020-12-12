b632973115a0b0d1cd10928cd93b6b6ec2a89858
Peter Seale <peter@pseale.com>
2020-12-11 20:01:53 -0600

    add template for p5.js sketches in Advent of Code

---
+++ src/_template_p5js/src/index.test.js
@@ -0,0 +1,8 @@
+const getInputs = require("./inputs");
+
+describe("(Part A)", () => {
+  test.todo("TODO");
+});
+
+let fs = require("fs");
+let realData = fs.readFileSync("./src/input.txt", "utf8");
---
+++ src/_template_p5js/src/inputs.js
@@ -0,0 +1,4 @@
+module.exports = function getInputs() {
+  // get stuff from the outside world
+  return {};
+};
