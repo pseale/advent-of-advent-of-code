696bf10a2b097cf597bffab2147b5eeb65e20f48
Peter Seale <peter@pseale.com>
2020-12-11 21:10:19 -0600

    refactor: procedural style

--- src/day11/src/sketch.js
+++ src/day11/src/sketch.js
@@ -5,6 +5,39 @@ let inputs;
 
 let width = window.innerWidth;
 let height = window.innerHeight;
+const gridSize = 60;
+const margin = gridSize * 0.7;
+
+function drawChair(p5, square, col, row) {
+  if (square === 0) {
+    drawGlyph(p5, "·", col, row);
+  } else if (square === 1) {
+    drawGlyph(p5, "🪑", col, row);
+  } else if (square === 2) {
+    drawGlyph(p5, "🧙‍♂️", col, row);
+  }
+}
+
+function drawGlyph(p5, glyph, col, row) {
+  p5.text(glyph, margin + col * gridSize, margin + row * gridSize);
+}
+
+const ticksPerFrame = 60;
+function drawGameOfLife(p5, tick) {
+  const frame = Math.floor(tick / ticksPerFrame);
+  if (frame > inputs.frames.length - 1) return;
+  const rows = inputs.frames[frame];
+
+  p5.textSize(50);
+  p5.textAlign(p5.CENTER, p5.CENTER);
+  for (let row = 0; row < rows.length; row++) {
+    for (let col = 0; col < rows[row].length; col++) {
+      const square = rows[row][col];
+
+      drawChair(p5, square, col, row);
+    }
+  }
+}
 
 let sketch = (p5) => {
   p5.setup = () => {
@@ -13,31 +46,10 @@ let sketch = (p5) => {
     p5.createCanvas(width, height);
   };
 
-  const ticksPerFrame = 60;
   let tick = 0;
   p5.draw = () => {
     tick++;
-    const frame = Math.floor(tick / ticksPerFrame);
-    if (frame > inputs.frames.length - 1) return;
-    const rows = inputs.frames[frame];
-
-    p5.textSize(50);
-    p5.textAlign(p5.CENTER, p5.CENTER);
-    const gridSize = 60;
-    const margin = gridSize * 0.7;
-    for (let row = 0; row < rows.length; row++) {
-      for (let col = 0; col < rows[row].length; col++) {
-        const square = rows[row][col];
-
-        if (square === 0) {
-          p5.text("·", margin + col * gridSize, margin + row * gridSize);
-        } else if (square === 1) {
-          p5.text("🪑", margin + col * gridSize, margin + row * gridSize);
-        } else if (square === 2) {
-          p5.text("🧙‍♂️", margin + col * gridSize, margin + row * gridSize);
-        }
-      }
-    }
+    drawGameOfLife(p5, tick);
   };
 };
 
