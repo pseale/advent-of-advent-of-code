2d32f6d356a54df9c823e8e6381accf7edd350ce
Peter Seale <peter@pseale.com>
2021-10-26 22:13:00 -0500

    add day 04 part a scaffolding

---
+++ AOAOC.Tests/Day04Tests.cs
@@ -0,0 +1,17 @@
+﻿using Day04;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day04Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            Assert.AreEqual(609043, Program.SolvePartA("abcdef"));
+            Assert.AreEqual(1048970, Program.SolvePartA("pqrstuv"));
+        }
+
+    }
+}
---
+++ Day04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day04
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // ReSharper disable once StringLiteralTypo
+            string input = "ckczppom";
+            
+            int partA = SolvePartA(input);
+            Console.WriteLine($"Hash is: {partA}");
+        }
+
+        public static int SolvePartA(string input)
+        {
+            return 0;
+        }
+    }
+}
