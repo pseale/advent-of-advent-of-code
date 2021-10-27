f4e0b4458c084d9c675bbda4bf32a7ec25e4e161
Peter Seale <peter@pseale.com>
2021-10-26 21:54:05 -0500

    solve day 03 part a; add scaffold for part b

---
+++ AOAOC.Tests/Day03Tests.cs
@@ -0,0 +1,25 @@
+﻿using Day03;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day03Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            Assert.AreEqual(2, Program.SolvePartA(">"));
+            Assert.AreEqual(4, Program.SolvePartA("^>v<"));
+            Assert.AreEqual(2, Program.SolvePartA("^v^v^v^v^v"));
+        }
+
+        [Test]
+        public void PartB()
+        {
+            Assert.AreEqual(3, Program.SolvePartB("^v"));
+            Assert.AreEqual(3, Program.SolvePartB("^>v<"));
+            Assert.AreEqual(11, Program.SolvePartB("^v^v^v^v^v"));
+        }
+    }
+}
--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Day03
@@ -10,11 +11,39 @@ namespace Day03
             var input = File.ReadAllText("input-part-a.txt");
             int partA = SolvePartA(input);
             Console.WriteLine($"Houses receiving at least one present: {partA}");
+
+            int partB = SolvePartB(input);
+            Console.WriteLine($"Houses receiving at least one present with Santa+Robo-Santa: {partA}");
         }
 
-        private static int SolvePartA(string input)
+        public static int SolvePartA(string input)
+        {
+            var visited = new HashSet<Location>();
+
+            int x = 0;
+            int y = 0;
+            visited.Add(new Location(x, y));
+
+            var moves = input.Trim().ToCharArray();
+            foreach (var move in moves)
+            {
+                if (move == '<') x--;
+                else if (move == '>') x++;
+                else if (move == 'v') y++;
+                else if (move == '^') y--;
+                else throw new Exception($"Invalid input: '{move}'");
+
+                visited.Add(new Location(x, y));
+            }
+
+            return visited.Count;
+        }
+
+        public static int SolvePartB(string input)
         {
             return 0;
         }
+
+        public record Location(int X, int Y);
     }
 }
