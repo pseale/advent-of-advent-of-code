1398e7b40c466490ebeeaf4bb8bf0002117b0762
Peter Seale <peter@pseale.com>
2021-10-26 20:30:34 -0500

    prepare day 02 part a

---
+++ AOAOC.Tests/Day02Tests.cs
@@ -0,0 +1,20 @@
+﻿using Day02;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day02Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            Assert.AreEqual(58, Program.SolvePartA("2x3x4"));
+
+            Assert.AreEqual(42, Program.SolvePartA("1x1x10"));
+
+            // test multiple lines, plus blank lines
+            Assert.AreEqual(42 + 58, Program.SolvePartA("1x1x10\r\n2x3x4\r\n"));
+        }
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Day02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputPartA = File.ReadAllText("input-part-a.txt");
+            var partA = SolvePartA(inputPartA);
+        }
+
+        public static int SolvePartA(string input)
+        {
+            Console.WriteLine(input);
+            return 0;
+        }
+    }
+
+
+}
