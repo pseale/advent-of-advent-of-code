e6f0f1d871b1f4a15ace93a52675bf6841ca20cb
Peter Seale <peter@pseale.com>
2021-10-26 22:23:23 -0500

    solve day 04 part a

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Day04
 {
@@ -8,14 +10,25 @@ namespace Day04
         {
             // ReSharper disable once StringLiteralTypo
             string input = "ckczppom";
-            
+
             int partA = SolvePartA(input);
             Console.WriteLine($"Hash is: {partA}");
         }
 
         public static int SolvePartA(string input)
         {
-            return 0;
+            using (var hasher = MD5.Create())
+            {
+                for (int i = 0; i < int.MaxValue; i++)
+                {
+                    var data = hasher.ComputeHash(ASCIIEncoding.ASCII.GetBytes(input + i));
+                    var hexadecimal = BitConverter.ToString(data).Replace("-", "");
+                    if (hexadecimal.StartsWith("00000"))
+                        return i;
+                }
+            }
+
+            throw new Exception("Exhausted all possible hashes--assume something is wrong with the code.");
         }
     }
 }
