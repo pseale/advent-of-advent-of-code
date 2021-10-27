9a5f1c1d61c9f7f730f3346dd302f85a2e079aa9
Peter Seale <peter@pseale.com>
2021-10-26 23:06:20 -0500

    solve day 05 part b

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -14,21 +14,11 @@ namespace Day05
 
             int partB = SolvePartB(input);
             Console.WriteLine($"Nice strings (better model): {partB}");
-
-        }
-
-        public static int SolvePartB(string input)
-        {
-            return 0;
         }
 
         public static int SolvePartA(string input)
         {
-            var lines = input
-                .Split("\n")
-                .Select(x => x.Trim())
-                .Where(x => !string.IsNullOrWhiteSpace(x))
-                .ToArray();
+            var lines = Parse(input);
 
             // ReSharper disable once ReplaceWithSingleCallToCount
             return lines.Where(x => HasThreeVowels(x))
@@ -38,6 +28,7 @@ namespace Day05
         }
 
         private static readonly char[] Vowels = new[] {'a', 'e', 'i', 'o', 'u'};
+
         private static bool HasThreeVowels(string @string)
         {
             // ReSharper disable once ReplaceWithSingleCallToCount
@@ -63,6 +54,55 @@ namespace Day05
             if (@string.Contains("xy")) return true;
 
             return false;
+        }
+
+        public static int SolvePartB(string input)
+        {
+            var lines = Parse(input);
+
+            // ReSharper disable once ReplaceWithSingleCallToCount
+            return lines.Where(x => HasPairWithoutOverlapping(x))
+                .Where(x => HasLetterSandwich(x))
+                .Count();
+        }
+
+        // I am aware this is inefficient
+        private static bool HasPairWithoutOverlapping(string @string)
+        {
+            for (int i = 0; i < @string.Length - 1; i++)
+            {
+                var pair = @string.Substring(i, 2);
+                var remaining = @string.Substring(i + 2);
+                if (remaining.Contains(pair))
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static bool HasLetterSandwich(string @string)
+        {
+            for (int i = 0; i < @string.Length - 2; i++)
+            {
+                var bread1 = @string[i];
+                // filling can be any letter
+                var bread2 = @string[i + 2];
+
+                if (bread1 == bread2)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static string[] Parse(string input)
+        {
+            var lines = input
+                .Split("\n")
+                .Select(x => x.Trim())
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToArray();
+            return lines;
         }
     }
 }
