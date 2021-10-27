50eabbc5436863a33533857eee5c5a22e13bc6db
Peter Seale <peter@pseale.com>
2021-10-26 22:07:24 -0500

    solve day 03 part b

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -41,7 +41,34 @@ namespace Day03
 
         public static int SolvePartB(string input)
         {
-            return 0;
+            var visited = new HashSet<Location>();
+
+            var start = new Location(0, 0);
+            var santaLocation = start;
+            var roboSantaLocation = start;
+
+            visited.Add(santaLocation);
+
+            var moves = input.Trim().ToCharArray();
+            for (int i = 0; i < moves.Length; i += 2)
+            {
+                santaLocation = Move(santaLocation, moves[i]);
+                roboSantaLocation = Move(roboSantaLocation, moves[i+1]);
+
+                visited.Add(santaLocation);
+                visited.Add(roboSantaLocation);
+            }
+
+            return visited.Count;
+        }
+
+        private static Location Move(Location location, char move)
+        {
+            if (move == '<') return new Location(location.X - 1, location.Y);
+            if (move == '>') return new Location(location.X + 1, location.Y);
+            if (move == 'v') return new Location(location.X, location.Y + 1);
+            if (move == '^') return new Location(location.X, location.Y - 1);
+            else throw new Exception($"Invalid input: '{move}'");
         }
 
         public record Location(int X, int Y);
