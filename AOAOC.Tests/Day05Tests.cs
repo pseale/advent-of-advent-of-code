872ae2e68cd1bdbd3fdbd66859b9d6397feeee2f
Peter Seale <peter@pseale.com>
2021-10-26 22:36:05 -0500

    add day 05 part a scaffolding

---
+++ AOAOC.Tests/Day05Tests.cs
@@ -0,0 +1,20 @@
+﻿using Day05;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day05Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            Assert.AreEqual(1, Program.SolvePartA("ugknbfddgicrmopn"));
+            Assert.AreEqual(1, Program.SolvePartA("aaa"));
+
+            Assert.AreEqual(0, Program.SolvePartA("jchzalrnumimnmhp"));
+            Assert.AreEqual(0, Program.SolvePartA("haegwjzuvuyypxyu"));
+            Assert.AreEqual(0, Program.SolvePartA("dvszwmarrgswjxmb"));
+        }
+    }
+}
---
+++ Day05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Day05
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            int partA = SolvePartA(input);
+            Console.WriteLine($"Nice strings: {partA}");
+        }
+
+        public static int SolvePartA(string input)
+        {
+            return 0;
+        }
+    }
+}
