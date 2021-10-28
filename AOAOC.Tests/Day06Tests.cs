db57b4f4e384432a21de01443dfb4ad42abd8f96
Peter Seale <peter@pseale.com>
2021-10-27 22:45:30 -0500

    solve day 06 part a, without refactoring

---
+++ AOAOC.Tests/Day06Tests.cs
@@ -0,0 +1,34 @@
+﻿using Day06;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day06Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            // making my own test cases, because their test cases involve 1000x1000 matrices
+            Assert.AreEqual(0, Program.SolvePartA(""));
+
+            // test very basic use case
+            Assert.AreEqual(1, Program.SolvePartA("toggle 0,0 through 0,0"));
+
+            // test that ranges work (very rudimentary)
+            Assert.AreEqual(2, Program.SolvePartA("toggle 0,0 through 0,1"));
+            Assert.AreEqual(2, Program.SolvePartA("toggle 0,0 through 1,0"));
+            Assert.AreEqual(4, Program.SolvePartA("toggle 0,0 through 1,1"));
+
+            // test 'toggle'
+            Assert.AreEqual(0, Program.SolvePartA("toggle 0,0 through 0,0\ntoggle 0,0 through 0,0"));
+
+            // test 'turn on'
+            Assert.AreEqual(2, Program.SolvePartA("turn on 0,0 through 0,1"));
+            Assert.AreEqual(2, Program.SolvePartA("turn on 0,0 through 0,1"));
+
+            // test 'turn off'
+            Assert.AreEqual(0, Program.SolvePartA("turn on 0,0 through 0,0\nturn off 0,0 through 0,0"));
+        }
+    }
+}
--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace Day06
 {
@@ -14,7 +15,86 @@ namespace Day06
 
         public static int SolvePartA(string input)
         {
-            return -1;
+            var lights = new bool[1000, 1000];
+
+            var lines = input
+                .Split("\n")
+                .Select(x => x.Trim())
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToArray();
+
+            foreach (var line in lines)
+            {
+                var words = line.Split(" ");
+                if (line.StartsWith("toggle"))
+                {
+                    Toggle(lights, ParsePoint(words[1]), ParsePoint(words[3]));
+                }
+                else if (line.StartsWith("turn on"))
+                {
+                    TurnOn(lights, ParsePoint(words[2]), ParsePoint(words[4]));
+                }
+                else if (line.StartsWith("turn off"))
+                {
+                    TurnOff(lights, ParsePoint(words[2]), ParsePoint(words[4]));
+                }
+                else
+                {
+                    throw new Exception($"Invalid input: '{line}'");
+                }
+            }
+
+            int lit = 0;
+            for (int y = 0; y < 1000; y++)
+            {
+                for (int x = 0; x < 1000; x++)
+                {
+                    if (lights[x, y])
+                        lit++;
+                }
+            }
+            return lit;
         }
+
+        private static void Toggle(bool[,] lights, Point topLeft, Point bottomRight)
+        {
+            for (int y = topLeft.Y; y <= bottomRight.Y; y++)
+            {
+                for (int x = topLeft.X; x <= bottomRight.X; x++)
+                {
+                    lights[x, y] = !lights[x, y];
+                }
+            }
+        }
+
+        private static void TurnOn(bool[,] lights, Point topLeft, Point bottomRight)
+        {
+            for (int y = topLeft.Y; y <= bottomRight.Y; y++)
+            {
+                for (int x = topLeft.X; x <= bottomRight.X; x++)
+                {
+                    lights[x, y] = true;
+                }
+            }
+        }
+
+        private static void TurnOff(bool[,] lights, Point topLeft, Point bottomRight)
+        {
+            for (int y = topLeft.Y; y <= bottomRight.Y; y++)
+            {
+                for (int x = topLeft.X; x <= bottomRight.X; x++)
+                {
+                    lights[x, y] = false;
+                }
+            }
+        }
+
+        private static Point ParsePoint(string coordinates)
+        {
+            var split = coordinates.Split(",");
+            return new Point(int.Parse(split[0]), int.Parse(split[1]));
+        }
+
+        record Point(int X, int Y);
     }
 }
