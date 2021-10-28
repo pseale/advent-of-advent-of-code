f574c3ba3c684a1e591eebb9f79f2fcb834286f1
Peter Seale <peter@pseale.com>
2021-10-28 00:09:48 -0500

    add day 07 part a scaffolding

---
+++ AOAOC.Tests/Day07Tests.cs
@@ -0,0 +1,34 @@
+﻿using Day07;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day07Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            var input = @"123 -> x
+                456 -> y
+                x AND y -> d
+                x OR y -> e
+                x LSHIFT 2 -> f
+                y RSHIFT 2 -> g
+                NOT x -> h
+                NOT y -> i";
+
+            var wires = Program.SolvePartA(input);
+
+            Assert.AreEqual(72, wires["d"]);
+            Assert.AreEqual(507, wires["e"]);
+            Assert.AreEqual(492, wires["f"]);
+            Assert.AreEqual(114, wires["g"]);
+            Assert.AreEqual(65412, wires["h"]);
+            Assert.AreEqual(65079, wires["i"]);
+            Assert.AreEqual(123, wires["x"]);
+            Assert.AreEqual(456, wires["y"]);
+        }
+
+    }
+}
---
+++ Day07/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day07
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            var partA = SolvePartA(input);
+            Console.WriteLine($"Signal provided to wire a: {partA["a"]}");
+        }
+
+        public static Dictionary<string, int> SolvePartA(string input)
+        {
+            return new Dictionary<string, int>();
+        }
+    }
+}
