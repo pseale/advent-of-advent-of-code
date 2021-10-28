0fde399c291192cc661c5e5b7829c0b97f3b4ffb
Peter Seale <peter@pseale.com>
2021-10-27 22:20:22 -0500

    add day 06 part a scaffolding

---
+++ Day06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Day06
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            var partA = SolvePartA(input);
+            Console.WriteLine($"Lit lights: {partA}");
+        }
+
+        public static int SolvePartA(string input)
+        {
+            return -1;
+        }
+    }
+}
