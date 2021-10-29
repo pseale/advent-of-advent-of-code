da34cf667bd9d03313dc6c85ad14c1f4c62b5519
Peter Seale <peter@pseale.com>
2021-10-29 00:27:03 -0500

    add scaffolding for day 09 part a

---
+++ AOAOC.Tests/Day09Tests.cs
@@ -0,0 +1,20 @@
+﻿using Day09;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day09Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            string distances = @"London to Dublin = 464
+                                London to Belfast = 518
+                                Dublin to Belfast = 141";
+
+            Assert.AreEqual(605, Program.SolvePartA(distances));
+        }
+
+    }
+}
