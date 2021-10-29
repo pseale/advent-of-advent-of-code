4818b63b34f54e6dc69014a8a7d4b35dc1d9cd26
Peter Seale <peter@pseale.com>
2021-10-28 20:53:38 -0500

    add day 08 scaffolding

---
+++ AOAOC.Tests/Day08Tests.cs
@@ -0,0 +1,23 @@
+﻿using Day08;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day08Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            Assert.AreEqual(2 - 0, Program.SolvePartA(Quote("")));
+            Assert.AreEqual(5 - 3, Program.SolvePartA(Quote("abc")));
+            Assert.AreEqual(10 - 7, Program.SolvePartA(Quote("aaa\\\"aaa"))); // aaa\"aaa
+            Assert.AreEqual(6 - 1, Program.SolvePartA(Quote("\\x27"))); // \x27
+        }
+
+        private string Quote(string s)
+        {
+            return '"' + s + '"';
+        }
+    }
+}
---
+++ Day08/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Day08
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            var partA = SolvePartA(input);
+            Console.WriteLine($"Difference between characters of code and characters in memory: {partA}");
+        }
+
+        public static int SolvePartA(string input)
+        {
+            return -1;
+        }
+    }
+}
