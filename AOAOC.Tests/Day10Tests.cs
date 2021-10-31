4af3de2bafbbee28a7187c32f50550cbdca71f28
Peter Seale <peter@pseale.com>
2021-10-31 18:01:31 -0500

    solve day 10 part a

--- AOAOC.Tests/Day10Tests.cs
+++ AOAOC.Tests/Day10Tests.cs
@@ -9,13 +9,14 @@ namespace AOAOC.Tests
         [Test]
         public void PartA()
         {
-            Assert.AreEqual(11, Program.SolvePartA(1, 1));
-            Assert.AreEqual(21, Program.SolvePartA(11, 1));
-            Assert.AreEqual(1211, Program.SolvePartA(21, 1));
-            Assert.AreEqual(111221, Program.SolvePartA(1211, 1));
-            Assert.AreEqual(312211, Program.SolvePartA(111211, 1));
+            Assert.AreEqual("11", Program.SolvePartA("1", 1));
+            Assert.AreEqual("21", Program.SolvePartA("11", 1));
+            Assert.AreEqual("1211", Program.SolvePartA("21", 1));
+            Assert.AreEqual("111221", Program.SolvePartA("1211", 1));
+            Assert.AreEqual("312211", Program.SolvePartA("111221", 1));
 
-            Assert.AreEqual(312211, Program.SolvePartA(1, 5));
+            Assert.AreEqual("312211", Program.SolvePartA("1", 5));
+            Assert.AreEqual("13112221", Program.SolvePartA("1", 6));
         }
     }
 }
--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day10
 {
@@ -6,15 +7,69 @@ namespace Day10
     {
         static void Main(string[] args)
         {
-            var input = 1113222113;
+            var input = "1113222113";
 
             var partA = SolvePartA(input, 40);
-            Console.WriteLine($"Distance of the shortest route: {partA}");
+            Console.WriteLine($"Length of the (very long) look-and-say result: {partA.Length}");
         }
 
-        public static int SolvePartA(int input, int iterations)
+        public static string SolvePartA(string input, int iterations)
         {
-            return -1;
+            string digits = input;
+
+            for (int i = 0; i < iterations; i++)
+                digits = LookAndSay(digits);
+
+            return digits;
+        }
+
+        private static string LookAndSay(string input)
+        {
+            var digits = input.ToCharArray();
+            var lookAndSay = new List<char>();
+
+            int index = 0;
+            while (index < digits.Length)
+            {
+                var runOfDigits = GetRunOfDigits(digits, index);
+
+                lookAndSay.Add(ConvertDigitToChar(runOfDigits));
+                lookAndSay.Add(digits[index]);
+
+                index += runOfDigits;
+            }
+
+            return new string(lookAndSay.ToArray());
+        }
+
+        private static int GetRunOfDigits(char[] digits, int startingIndex)
+        {
+            int index = startingIndex + 1;
+            int run = 1;
+
+            while (index < digits.Length)
+            {
+                if (digits[index] == digits[startingIndex])
+                {
+                    run++;
+                    index++;
+                }
+                else
+                {
+                    return run;
+                }
+            }
+
+            return run;
+        }
+
+        private static char ConvertDigitToChar(int digit)
+        {
+            if (digit <= 0) throw new Exception($"Invalid digit: {digit}");
+            if (digit > 9) throw new Exception($"Didn't expect digit this large: {digit}");
+
+            return (char) (digit + (int) '0');
+
         }
     }
 }
