924a7ba44c881c2e8dd76690235c06eb8d74c814
Peter Seale <peter@pseale.com>
2021-10-31 18:09:09 -0500

    add scaffolding for day 11 part a

---
+++ AOAOC.Tests/Day11Tests.cs
@@ -0,0 +1,17 @@
+﻿using Day11;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day11Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            // end-to-end
+            Assert.AreEqual("abcdffaa", Program.SolvePartA("abcdefgh"));
+            Assert.AreEqual("ghjaabcc", Program.SolvePartA("ghijklmn"));
+        }
+    }
+}
---
+++ Day11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day11
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "cqjxjnds";
+
+            var partA = SolvePartA(input);
+            Console.WriteLine($"Santa's next password should be: {partA}");
+        }
+
+        public static string SolvePartA(string input)
+        {
+            return "hunter2";
+        }
+    }
+}
