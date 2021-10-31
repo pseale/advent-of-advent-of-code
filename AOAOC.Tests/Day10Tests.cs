0fb004cded8dfb3f2f0a394517bf5731acd25ab0
Peter Seale <peter@pseale.com>
2021-10-31 16:53:27 -0500

    add scaffolding for day 10

---
+++ AOAOC.Tests/Day10Tests.cs
@@ -0,0 +1,21 @@
+﻿using Day10;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day10Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            Assert.AreEqual(11, Program.SolvePartA(1, 1));
+            Assert.AreEqual(21, Program.SolvePartA(11, 1));
+            Assert.AreEqual(1211, Program.SolvePartA(21, 1));
+            Assert.AreEqual(111221, Program.SolvePartA(1211, 1));
+            Assert.AreEqual(312211, Program.SolvePartA(111211, 1));
+
+            Assert.AreEqual(312211, Program.SolvePartA(1, 5));
+        }
+    }
+}
---
+++ Day10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day10
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = 1113222113;
+
+            var partA = SolvePartA(input, 40);
+            Console.WriteLine($"Distance of the shortest route: {partA}");
+        }
+
+        public static int SolvePartA(int input, int iterations)
+        {
+            return -1;
+        }
+    }
+}
