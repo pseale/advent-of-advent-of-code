e4b30cdff494a4e5a802e7915638b5002c20034b
Peter Seale <peter@pseale.com>
2021-10-31 19:51:53 -0500

    add scaffolding for day 12 part a

---
+++ AOAOC.Tests/Day12Tests.cs
@@ -0,0 +1,23 @@
+﻿using Day12;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day12Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            Assert.AreEqual(6, Program.SolvePartA("[1,2,3]"));
+            Assert.AreEqual(6, Program.SolvePartA("{\"a\":2,\"b\":4}"));
+
+            Assert.AreEqual(3, Program.SolvePartA("[[[3]]]"));
+            Assert.AreEqual(3, Program.SolvePartA("{\"a\":{\"b\":4},\"c\":-1}"));
+            Assert.AreEqual(0, Program.SolvePartA("{\"a\":[-1,1]}"));
+            Assert.AreEqual(0, Program.SolvePartA("[-1,{\"a\":1}]"));
+            Assert.AreEqual(0, Program.SolvePartA("[]"));
+            Assert.AreEqual(0, Program.SolvePartA("{}"));
+        }
+    }
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Day12
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.json");
+
+            var partA = SolvePartA(input);
+            Console.WriteLine($"Sum of all numbers: {partA}");
+        }
+
+        public static int SolvePartA(string input)
+        {
+            return -1;
+        }
+    }
+}
