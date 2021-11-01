c723729b08b680d2dfb0053919e7743e897161d4
Peter Seale <peter@pseale.com>
2021-10-31 22:59:58 -0500

    add scaffolding for day 14 part a

---
+++ AOAOC.Tests/Day14Tests.cs
@@ -0,0 +1,49 @@
+﻿using Day14;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day14Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            Assert.AreEqual(14, Program.DistanceTraveled(
+                "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+                1));
+            Assert.AreEqual(16, Program.DistanceTraveled(
+                "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
+                1));
+
+            Assert.AreEqual(140, Program.DistanceTraveled(
+                "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+                10));
+            Assert.AreEqual(160, Program.DistanceTraveled(
+                "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
+                10));
+
+            Assert.AreEqual(140, Program.DistanceTraveled(
+                "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+                11));
+            Assert.AreEqual(176, Program.DistanceTraveled(
+                "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
+                11));
+
+            Assert.AreEqual(140, Program.DistanceTraveled(
+                "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+                12));
+            Assert.AreEqual(176, Program.DistanceTraveled(
+                "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
+                12));
+
+            Assert.AreEqual(1120, Program.DistanceTraveled(
+                "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.",
+                1000));
+            Assert.AreEqual(1056, Program.DistanceTraveled(
+                "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.",
+                1000));
+        }
+
+    }
+}
---
+++ Day14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Day14
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            var partA = SolvePartA(input, 2503);
+            Console.WriteLine($"Total change in happiness for the optimal seating arrangement: {partA}");
+        }
+
+        private static int SolvePartA(string input, int seconds)
+        {
+            return -1;
+        }
+
+        public static int DistanceTraveled(string line, int seconds)
+        {
+            return -1;
+        }
+    }
+}
