a0abb29bcf72ee29500d4346573c283e7192e4b8
Peter Seale <peter@pseale.com>
2021-10-31 20:43:55 -0500

    add scaffolding for day 13 part a

---
+++ Day13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Day13
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            var partA = SolvePartA(input);
+            Console.WriteLine($"Total change in happiness for the optimal seating arrangement: {partA}");
+        }
+
+        public static int SolvePartA(string input)
+        {
+            return -1;
+        }
+    }
+}
