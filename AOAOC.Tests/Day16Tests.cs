46edb35c0ae60208723047d6965ecd087ef53411
Peter Seale <peter@pseale.com>
2021-11-01 19:48:29 -0500

    add scaffolding for day 16

---
+++ AOAOC.Tests/Day16Tests.cs
@@ -0,0 +1,34 @@
+﻿using Day16;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day16Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            // fine, I'll make my own test cases
+            var compounds = Program.ParseCompounds(@"children: 3
+                cats: 7
+                samoyeds: 2
+                pomeranians: 3
+                akitas: 0
+                vizslas: 0
+                goldfish: 5
+                trees: 3
+                cars: 2
+                perfumes: 1".Replace("\n",","));
+
+            // match when only one matches
+            Assert.AreEqual(1, Program.SolvePartA("Sue 1: cats: 7\nSue 777: cats: 6"));
+
+            // match when everything matches
+            Assert.AreEqual(1, Program.SolvePartA("Sue 1: children: 3, cats: 7, samoyeds: 2, pomeranians: 3, akitas: 0, vizslas: 0, goldfish: 5, trees: 3, cars: 2, perfumes: 1\nSue 777: cats: 6"));
+
+            // match in a realistic situation with 3 known properties, and 2 of which match for most Sues
+            Assert.AreEqual(1, Program.SolvePartA("Sue 1: cats: 7, samoyeds: 2, pomeranians: 3\nSue 777: cats: 6, akitas: 0, vizslas: 0"));
+        }
+    }
+}
---
+++ Day16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day16
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            var partA = SolvePartA(input);
+            Console.WriteLine($"Aunt Sue #: {partA}");
+        }
+
+        public static int SolvePartA(string input)
+        {
+            return -1;
+        }
+
+        public static Dictionary<string, int> ParseCompounds(string input)
+        {
+            var compounds = new Dictionary<string, int>();
+
+            var chunks = input.Split(",");
+            foreach (var chunk in chunks)
+            {
+                var words = chunk.Split(" ").Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+                var name = words[0].TrimEnd(':').Trim();
+                var value = int.Parse(words[1].Trim());
+                compounds.Add(name, value);
+            }
+
+            return compounds;
+        }
+    }
+
+    public record Sue(int SueNumber, Dictionary<string, int> Compounds);
+}
