6c9ae46c6b6c1af5d983f94d583c31bde27d53c3
Peter Seale <peter@pseale.com>
2021-11-01 20:11:45 -0500

    solve day 15 part a - as often happens, unit tests written in bulk beforehand often end up invalid before the feature is written

--- AOAOC.Tests/Day16Tests.cs
+++ AOAOC.Tests/Day16Tests.cs
@@ -10,22 +10,7 @@ namespace AOAOC.Tests
         public void PartA()
         {
             // fine, I'll make my own test cases
-            var compounds = Program.ParseCompounds(@"children: 3
-                cats: 7
-                samoyeds: 2
-                pomeranians: 3
-                akitas: 0
-                vizslas: 0
-                goldfish: 5
-                trees: 3
-                cars: 2
-                perfumes: 1".Replace("\n",","));
-
-            // match when only one matches
-            Assert.AreEqual(1, Program.SolvePartA("Sue 1: cats: 7\nSue 777: cats: 6"));
-
-            // match when everything matches
-            Assert.AreEqual(1, Program.SolvePartA("Sue 1: children: 3, cats: 7, samoyeds: 2, pomeranians: 3, akitas: 0, vizslas: 0, goldfish: 5, trees: 3, cars: 2, perfumes: 1\nSue 777: cats: 6"));
+            var compounds = Program.GetMfcsamOutput();
 
             // match in a realistic situation with 3 known properties, and 2 of which match for most Sues
             Assert.AreEqual(1, Program.SolvePartA("Sue 1: cats: 7, samoyeds: 2, pomeranians: 3\nSue 777: cats: 6, akitas: 0, vizslas: 0"));
--- Day16/Program.cs
+++ Day16/Program.cs
@@ -17,7 +17,40 @@ namespace Day16
 
         public static int SolvePartA(string input)
         {
-            return -1;
+            var sues = Parse(input);
+
+            var mfcsamOutput = GetMfcsamOutput();
+            return sues.Single(x => Matches(mfcsamOutput, x)).SueNumber;
+        }
+
+        private static bool Matches(Dictionary<string,int> compounds, Sue sue)
+        {
+            return sue.Compounds.All(compound =>
+                compounds.ContainsKey(compound.Key) && compounds[compound.Key] == compound.Value);
+        }
+
+        private static List<Sue> Parse(string input)
+        {
+            var lines = input
+                .Split("\n")
+                .Select(x => x.Trim())
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToArray();
+
+            var parsed = new List<Sue>();
+            foreach (var line in lines)
+            {
+                var words = line.Split(" ");
+                var sueNumber = int.Parse(words[1].TrimEnd(':'));
+                var compounds = new Dictionary<string, int>();
+                // Apology: late is the hour (I could have done something to make this dynamic)
+                compounds.Add(words[2].TrimEnd(':'), int.Parse(words[3].TrimEnd(',')));
+                compounds.Add(words[4].TrimEnd(':'), int.Parse(words[5].TrimEnd(',')));
+                compounds.Add(words[6].TrimEnd(':'), int.Parse(words[7].TrimEnd(',')));
+                parsed.Add(new Sue(sueNumber, compounds));
+            }
+
+            return parsed;
         }
 
         public static Dictionary<string, int> ParseCompounds(string input)
@@ -25,6 +58,26 @@ namespace Day16
             var compounds = new Dictionary<string, int>();
 
             var chunks = input.Split(",");
+            foreach (var chunk in chunks)
+            {
+                var words = chunk.Split(" ").Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+                var name = words[0].TrimEnd(':').Trim();
+                var value = int.Parse(words[1].Trim());
+                compounds.Add(name, value);
+            }
+
+            return compounds;
+        }
+
+        public static Dictionary<string, int> GetMfcsamOutput()
+        {
+            var compounds = new Dictionary<string, int>();
+
+            // ReSharper disable StringLiteralTypo
+            var chunks = @"children: 3, cats: 7, samoyeds: 2, pomeranians: 3, akitas: 0, vizslas: 0, goldfish: 5, trees: 3, cars: 2, perfumes: 1"
+                .Split(",");
+            // ReSharper restore StringLiteralTypo
+
             foreach (var chunk in chunks)
             {
                 var words = chunk.Split(" ").Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
