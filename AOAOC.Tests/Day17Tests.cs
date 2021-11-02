901cf24b39a260bd192c07cce9d8778a16a474df
Peter Seale <peter@pseale.com>
2021-11-01 21:04:43 -0500

    add scaffolding for day 17 part a

---
+++ AOAOC.Tests/Day17Tests.cs
@@ -0,0 +1,17 @@
+﻿using Day17;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day17Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            var input = @"20 \n 15 \n 10 \n 5 \n 5";
+
+            Assert.AreEqual(4, Program.SolvePartA(input));
+        }
+    }
+}
---
+++ Day17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Day17
+{
+    public static class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            var partA = SolvePartA(input);
+            Console.WriteLine($"Combinations of containers: {partA}");
+        }
+
+        public static int SolvePartA(string input)
+        {
+            return -1;
+        }
+    }
+}
