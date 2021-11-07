c79ecc566dfe07a1af92cf3cb10cfd617bdf3786
Peter Seale <peter@pseale.com>
2021-11-07 13:57:49 -0600

    through much trial and deep suffering, we have solved day 17 part a

--- AOAOC.Tests/Day17Tests.cs
+++ AOAOC.Tests/Day17Tests.cs
@@ -1,4 +1,5 @@
-﻿using Day17;
+﻿using System.Collections.Generic;
+using Day17;
 using NUnit.Framework;
 
 namespace AOAOC.Tests
@@ -9,9 +10,34 @@ namespace AOAOC.Tests
         [Test]
         public void PartA()
         {
-            var input = @"20 \n 15 \n 10 \n 5 \n 5";
+            var input = "20 \n 15 \n 10 \n 5 \n 5";
 
-            Assert.AreEqual(4, Program.SolvePartA(input));
+            Assert.AreEqual(4, Program.SolvePartA(input, 20));
+        }
+
+        [Test]
+        public void ShouldWorkWithSingleContainer()
+        {
+            var input = "20";
+
+            Assert.AreEqual(1, Program.SolvePartA(input, 20));
+        }
+
+        [Test]
+        public void ShouldWorkWithSingleContainerThatFits()
+        {
+            // only the 20 liter container will fit
+            var input = "20 \n 30 \n 40 \n 50";
+
+            Assert.AreEqual(1, Program.SolvePartA(input, 20));
+        }
+
+        [Test]
+        public void ShouldWorkWithTwoContainers()
+        {
+            var input = new List<int>() { 10, 10 };
+
+            CollectionAssert.AreEqual(new List<string>() {"-0-1"}, Program.Combinations(input, 20));
         }
     }
 }
--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace Day17
 {
@@ -10,13 +13,55 @@ namespace Day17
         {
             var input = File.ReadAllText("input.txt");
 
-            var partA = SolvePartA(input);
+            var partA = SolvePartA(input, 150);
             Console.WriteLine($"Combinations of containers: {partA}");
         }
 
-        public static int SolvePartA(string input)
+        public static int SolvePartA(string input, int target)
         {
-            return -1;
+            var lines = input
+                .Split("\n")
+                .Select(x => x.Trim())
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .ToArray();
+
+            var containers = lines.Select(line => int.Parse(line)).ToList();
+            return Combinations(containers, target)
+                .Count();
+        }
+
+        public static IEnumerable<string> Combinations(List<int> containers, int target)
+        {
+            return CombinationsRecursive(containers, target, "", 0)
+                .ToArray()
+                .Distinct()
+                .ToArray();
+        }
+
+        // EDITOR'S NOTE: this one took a lot of effort to get right. I am not accustomed to writing
+        // recursive combinatorial algorithms.
+        private static IEnumerable<string> CombinationsRecursive(List<int> containers, int target, string partialCombination, int loop)
+        {
+            if (loop == containers.Count)
+            {
+                var sum = partialCombination
+                    .Split("-")
+                    .Where(x => !string.IsNullOrWhiteSpace(x))
+                    .Sum(x => containers[int.Parse(x)]);
+                if (sum == target)
+                    yield return partialCombination;
+            }
+            else
+            {
+                // try a combination where we use this container
+                var newPartialCombination = $"{partialCombination}-{loop}";
+                foreach (var combination in CombinationsRecursive(containers, target, newPartialCombination, loop + 1))
+                    yield return combination;
+
+                // try a combination where we do NOT use this container
+                foreach (var combination in CombinationsRecursive(containers, target, partialCombination, loop + 1))
+                    yield return combination;
+            }
         }
     }
 }
