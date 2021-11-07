23c1078d78c1acaa44e92c0dc3585bfd6eed71b2
Peter Seale <peter@pseale.com>
2021-11-07 17:03:42 -0600

    add day 18 part a test harness

---
+++ AOAOC.Tests/Day18Tests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using Day18;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day18Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            var input = @".#.#.#
+                          ...##.
+                          #....#
+                          ..#...
+                          #.#..#
+                          ####..";
+
+            var frames = Day18Solution.GetFrames(input);
+
+            var expectedFrames = new List<string>() {
+@".#.#.#
+...##.
+#....#
+..#...
+#.#..#
+####..",
+
+
+@"..##..
+..##.#
+...##.
+......
+#.....
+#.##..",
+
+@"..###.
+......
+..###.
+......
+.#....
+.#....",
+
+@"...#..
+......
+...#..
+..##..
+......
+......",
+
+@"......
+......
+..##..
+..##..
+......
+......"
+            };
+
+            CollectionAssert.AreEqual(expectedFrames, frames);
+        }
+    }
+}
--- Day18/Day18Solution.cs
+++ Day18/Day18Solution.cs
@@ -1,9 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Day18
 {
     public static class Day18Solution
     {
+        public static List<string> GetFrames(string input)
+        {
+            var lines = input.Split("`n")
+                .Select(x => x.Trim())
+                .ToArray();
+
+            return new List<string>();
+        }
+
         public static List<string> GetDummyFrames()
         {
             return new List<string>()
