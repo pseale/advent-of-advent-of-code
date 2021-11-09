aeb57bcdbc45c2e05b2911ffd965d2dfd8ec53a0
Peter Seale <peter@pseale.com>
2021-11-08 18:20:16 -0600

    add scaffolding for day 20 part a

---
+++ AOAOC.Tests/Day20Tests.cs
@@ -0,0 +1,23 @@
+﻿using Day20;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day20Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            Assert.AreEqual(10, Program.SolvePartA(1));
+            Assert.AreEqual(30, Program.SolvePartA(2));
+            Assert.AreEqual(40, Program.SolvePartA(3));
+            Assert.AreEqual(70, Program.SolvePartA(4));
+            Assert.AreEqual(60, Program.SolvePartA(5));
+            Assert.AreEqual(120, Program.SolvePartA(6));
+            Assert.AreEqual(80, Program.SolvePartA(7));
+            Assert.AreEqual(150, Program.SolvePartA(8));
+            Assert.AreEqual(130, Program.SolvePartA(9));
+        }
+    }
+}
---
+++ Day20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day20
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = 34000000;
+
+            var partA = SolvePartA(input);
+            Console.WriteLine($"Distinct molecules: {partA}");
+        }
+
+        public static int SolvePartA(int input)
+        {
+            return -1;
+        }
+    }
+}
