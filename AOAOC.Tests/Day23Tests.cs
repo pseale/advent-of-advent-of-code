3267741b953dcb72d585bbef5e0fcbbf063f4b7c
Peter Seale <peter@pseale.com>
2021-11-16 21:25:44 -0600

    add scaffolding for day 23 part a

---
+++ AOAOC.Tests/Day23Tests.cs
@@ -0,0 +1,22 @@
+﻿using Day23;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day23Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            var input = @"inc a
+                          jio a, +2
+                          tpl a
+                          inc a";
+
+            var result = Program.SolvePartA(input, "a");
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ Day23/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Day23
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            var partA = SolvePartA(input, "b");
+            Console.WriteLine($"Value of register b: {partA}");
+        }
+
+        public static int SolvePartA(string input, string register)
+        {
+            return -1;
+        }
+    }
+}
