88d5d4481395578a218ddccdcaa9aab6b66140d2
Peter Seale <peter@pseale.com>
2021-11-18 19:34:30 -0600

    add scaffolding for day 24 part a

---
+++ AOAOC.Tests/Day24Tests.cs
@@ -0,0 +1,28 @@
+﻿using Day24;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day24Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            var input = @"1
+                          2
+                          3
+                          4
+                          5
+                          7
+                          8
+                          9
+                          10
+                          11";
+
+            var result = Program.SolvePartA(input);
+
+            Assert.AreEqual(90, result);
+        }
+    }
+}
---
+++ Day24/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Day24
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            var partA = SolvePartA(input);
+            Console.WriteLine($"Quantum entanglement of the first group of packages: {partA}");
+        }
+
+        public static int SolvePartA(string input)
+        {
+            return -1;
+        }
+    }
+}
