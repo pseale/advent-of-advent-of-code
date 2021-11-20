c01c66834318e544bedce116c36be5ee7dfa9222
Peter Seale <peter@pseale.com>
2021-11-19 23:16:09 -0600

    add scaffolding for day 25 part a

---
+++ AOAOC.Tests/Day25Tests.cs
@@ -0,0 +1,17 @@
+﻿using Day25;
+using NUnit.Framework;
+
+namespace AOAOC.Tests
+{
+    [TestFixture]
+    public class Day25Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            var topLeftCode = 1;
+
+            Assert.AreEqual(2, Program.SolvePartA(topLeftCode, 2, 1));
+        }
+    }
+}
---
+++ Day25/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Day25
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var partA = SolvePartA(20151125, 2947, 3029);
+            Console.WriteLine($"Code: {partA}");
+        }
+
+        private static int SolvePartA(int topLeftCode, int row, int column)
+        {
+            return -1;
+        }
+    }
+}
