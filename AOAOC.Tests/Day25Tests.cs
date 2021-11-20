e7bdff87b88651df2826d9c4287803cc635fd36f
Peter Seale <peter@pseale.com>
2021-11-19 23:46:57 -0600

    solve day 25 part a (there is no part b)

--- AOAOC.Tests/Day25Tests.cs
+++ AOAOC.Tests/Day25Tests.cs
@@ -21,21 +21,20 @@ namespace AOAOC.Tests
             // 6 | 16
 
             // top row (row #1)
-            Assert.AreEqual(1, Program.SolvePartA(topLeftCode, 1, 1));
-            Assert.AreEqual(3, Program.SolvePartA(topLeftCode, 2, 1));
-            Assert.AreEqual(6, Program.SolvePartA(topLeftCode, 3, 1));
-            Assert.AreEqual(10, Program.SolvePartA(topLeftCode, 4, 1));
-            Assert.AreEqual(15, Program.SolvePartA(topLeftCode, 5, 1));
-            Assert.AreEqual(21, Program.SolvePartA(topLeftCode, 6, 1));
+            Assert.AreEqual(3, Program.GetCodeFor(topLeftCode, 2, 1, x => x+1));
+            Assert.AreEqual(6, Program.GetCodeFor(topLeftCode, 3, 1, x => x+1));
+            Assert.AreEqual(10, Program.GetCodeFor(topLeftCode, 4, 1, x => x+1));
+            Assert.AreEqual(15, Program.GetCodeFor(topLeftCode, 5, 1, x => x+1));
+            Assert.AreEqual(21, Program.GetCodeFor(topLeftCode, 6, 1, x => x+1));
 
             // lazy spot checking now
-            Assert.AreEqual(2, Program.SolvePartA(topLeftCode, 1, 2));
-            Assert.AreEqual(20, Program.SolvePartA(topLeftCode, 5, 2));
+            Assert.AreEqual(2, Program.GetCodeFor(topLeftCode, 1, 2, x => x+1));
+            Assert.AreEqual(20, Program.GetCodeFor(topLeftCode, 5, 2, x => x+1));
 
-            Assert.AreEqual(4, Program.SolvePartA(topLeftCode, 1, 3));
-            Assert.AreEqual(19, Program.SolvePartA(topLeftCode, 4, 3));
+            Assert.AreEqual(4, Program.GetCodeFor(topLeftCode, 1, 3, x => x+1));
+            Assert.AreEqual(19, Program.GetCodeFor(topLeftCode, 4, 3, x => x+1));
 
-            Assert.AreEqual(16, Program.SolvePartA(topLeftCode, 1, 6));
+            Assert.AreEqual(16, Program.GetCodeFor(topLeftCode, 1, 6, x => x+1));
         }
     }
 }
--- Day25/Program.cs
+++ Day25/Program.cs
@@ -11,9 +11,41 @@ namespace Day25
             Console.WriteLine($"Code: {partA}");
         }
 
-        public static int SolvePartA(int topLeftCode, int column, int row)
+        private static long SolvePartA(long topLeftCode, int targetColumn, int targetRow)
         {
-            return -1;
+            const long multiplyBy = 252533;
+            const long moduloBy = 33554393;
+
+            Func<long, long> calculation = (c) => (c * multiplyBy) % moduloBy;
+
+            return GetCodeFor(topLeftCode, targetColumn, targetRow, calculation);
+        }
+
+        public static long GetCodeFor(long topLeftCode, int targetColumn, int targetRow, Func<long, long> calculation)
+        {
+            long code = topLeftCode;
+            var col = 1;
+            var row = 1;
+            while (true)
+            {
+                if (row == 1)
+                {
+                    // do a carriage return + line feed - back to column 1, row = current column + 1
+                    row = col + 1;
+                    col = 1;
+                }
+                else
+                {
+                    // move up one row, one column to the right
+                    row -= 1;
+                    col += 1;
+                }
+
+                code = calculation(code);
+
+                if (col == targetColumn && row == targetRow)
+                    return code;
+            }
         }
     }
 }
