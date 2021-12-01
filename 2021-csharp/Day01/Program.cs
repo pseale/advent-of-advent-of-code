8e347c3af4310edb060d35f587a266da6ae130db
Peter Seale <peter@pseale.com>
2021-12-01 14:01:04 -0600

    add scaffolding for day 01 part a

---
+++ 2021-csharp/Day01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Day01
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+            var partA = SolvePartA(input);
+            Console.WriteLine($"Measurements larger than the previous measurement: {partA}");
+        }
+
+        public static int SolvePartA(string input)
+        {
+            return -1;
+        }
+    }
+}
---
+++ 2021-csharp/Tests/Day01Tests.cs
@@ -0,0 +1,25 @@
+using Day01;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Day01Tests
+    {
+        [Test]
+        public void PartA()
+        {
+            var input = @"199
+                          200
+                          208
+                          210
+                          200
+                          207
+                          240
+                          269
+                          260
+                          263";
+
+            Assert.AreEqual(7, Program.SolvePartA(input));
+        }
+    }
+}
