2f0d0dc9ed1e1416fea0c29593e8a24575f5e7ba
Peter Seale <peter@pseale.com>
2021-12-01 16:25:20 -0600

    solve 2015 day 20 part b - with help!

--- 2015-csharp/Day20/Program.cs
+++ 2015-csharp/Day20/Program.cs
@@ -10,10 +10,17 @@ namespace Day20
         {
             var input = 34000000;
 
+            Console.WriteLine("WARNING: this takes roughly a minute to complete.");
+            Console.WriteLine();
+
             var partA = SolvePartA(input);
             Console.WriteLine($"Lowest house number: {partA}");
+
+            var partB = SolvePartB(input);
+            Console.WriteLine($"Lowest house number (updated rules): {partB}");
         }
 
+        // https://codeshare.io/vwXeNy thanks!
         public static int SolvePartA(int input)
         {
             var lowerBound = 1;
@@ -28,6 +35,34 @@ namespace Day20
             }
 
             throw new Exception("Couldn't find a house");
+        }
+
+        // thanks https://codeshare.io/vwXeNy !
+        private static int SolvePartB(int input)
+        {
+            var lowerBound = 1;
+            var upperBound = input;
+            for (int houseNumber = lowerBound; houseNumber <= upperBound; houseNumber++)
+            {
+                var visitingElves = GetDivisors(houseNumber)
+                    .Where(x => ElfWillVisit(x, houseNumber))
+                    .ToArray();
+                var packagesDelivered = visitingElves.Select(x => x * 11).Sum();
+                if (packagesDelivered >= input)
+                    return houseNumber;
+            }
+
+            throw new Exception("Couldn't find a house");
+        }
+
+        // apology: broke out this method because I could not hold the logic in my head otherwise
+        private static bool ElfWillVisit(int giftingHouse, int recipientHouse)
+        {
+            // filter out gifting houses that have already visited 50 houses
+            if (giftingHouse * 50 < recipientHouse)
+                return false;
+
+            return true;
         }
 
         // https://codereview.stackexchange.com/a/237490
