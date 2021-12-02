43bafb673a8f53989bafd2426f8e7e444cd1b346
Peter Seale <peter@pseale.com>
2021-12-02 10:44:54 -0600

    add scaffolding for day 02 part a

---
+++ 2021-csharp/Day02/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Day02;
+
+public static class Program
+{
+    static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+        var partA = SolvePartA(input);
+        Console.WriteLine($"Multiplied final horizontal position by final depth: {partA}");
+    }
+
+    public static int SolvePartA(string input)
+    {
+        return -1;
+    }
+}
---
+++ 2021-csharp/Tests/Day02Tests.cs
@@ -0,0 +1,21 @@
+﻿using Day02;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class Day02Tests
+{
+    [Test]
+    public void PartA()
+    {
+        var input = @"forward 5
+                      down 5
+                      forward 8
+                      up 3
+                      down 8
+                      forward 2";
+
+        Assert.AreEqual(15*10, Program.SolvePartA(input));
+    }
+}
