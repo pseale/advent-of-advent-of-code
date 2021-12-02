c637e5efe80dd5080cab2157d49d447c3fd5c028
Peter Seale <peter@pseale.com>
2021-12-02 10:57:45 -0600

    solve day 02 part a

--- 2021-csharp/Day02/Program.cs
+++ 2021-csharp/Day02/Program.cs
@@ -13,6 +13,42 @@ public static class Program
 
     public static int SolvePartA(string input)
     {
-        return -1;
+        var lines = input
+            .Split("\n")
+            .Select(x => x.Trim())
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .ToArray();
+
+        var instructions = new List<SubmarineInstruction>();
+        foreach (var line in lines)
+        {
+            var words = line.Split(" ");
+            instructions.Add(new SubmarineInstruction(words[0], int.Parse(words[1])));
+        }
+
+        int depth = 0;
+        int horizontalPosition = 0;
+        foreach (var instruction in instructions)
+        {
+            switch (instruction.Direction)
+            {
+                case "forward":
+                    horizontalPosition += instruction.Units;
+                    break;
+                case "up":
+                    depth -= instruction.Units;
+                    if (depth < 0)
+                        throw new Exception("Depth should never go negative! Submarines can't fly.");
+                    break;
+                case "down":
+                    depth += instruction.Units;
+                    break;
+                default:
+                    throw new Exception($"Invalid submarine instruction: {instruction.Direction}");
+            }
+        }
+        return depth * horizontalPosition;
     }
+
+    public record SubmarineInstruction(string Direction, int Units);
 }
