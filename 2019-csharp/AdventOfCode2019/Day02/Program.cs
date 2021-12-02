7c28824d1ce7f6ebba28691d5c9fe0000c4177ca
Peter Seale <peter@pseale.com>
2021-12-02 12:26:32 -0600

    add scaffolding for 2019 day 02 part a - intcode problem #1

---
+++ 2019-csharp/AdventOfCode2019/Day02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day02;
+
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = File.ReadAllText("input.txt");
+        var partA = SolvePartA(input);
+        Console.WriteLine($"100 * noun + verb: {partA}");
+    }
+
+    public static int SolvePartA(string input)
+    {
+        return -1;
+    }
+}
---
+++ 2019-csharp/AdventOfCode2019/Tests/Day02Tests.cs
@@ -0,0 +1,31 @@
+using Day02;
+using NUnit.Framework;
+
+namespace Tests;
+
+public class Day02Tests
+{
+    [Test]
+    public void PartASampleData()
+    {
+        // main example
+        var input = "1,9,10,3,2,3,11,0,99,30,40,50";
+
+        Assert.AreEqual(3500, Program.SolvePartA(input));
+
+        // smaller samples
+        Assert.AreEqual(2, Program.SolvePartA("1,0,0,0,99"));
+        Assert.AreEqual(2, Program.SolvePartA("2,3,0,3,99"));
+        Assert.AreEqual(2, Program.SolvePartA("2,4,4,5,99,0"));
+        Assert.AreEqual(30, Program.SolvePartA("1,1,1,4,99,5,6,0,99"));
+    }
+
+    [Test]
+    public void PartARealData()
+    {
+        // main example
+        var input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,19,5,23,1,13,23,27,1,27,6,31,2,31,6,35,2,6,35,39,1,39,5,43,1,13,43,47,1,6,47,51,2,13,51,55,1,10,55,59,1,59,5,63,1,10,63,67,1,67,5,71,1,71,10,75,1,9,75,79,2,13,79,83,1,9,83,87,2,87,13,91,1,10,91,95,1,95,9,99,1,13,99,103,2,103,13,107,1,107,10,111,2,10,111,115,1,115,9,119,2,119,6,123,1,5,123,127,1,5,127,131,1,10,131,135,1,135,6,139,1,10,139,143,1,143,6,147,2,147,13,151,1,5,151,155,1,155,5,159,1,159,2,163,1,163,9,0,99,2,14,0,0";
+
+        Assert.AreEqual(4690667, Program.SolvePartA(input));
+    }
+}
