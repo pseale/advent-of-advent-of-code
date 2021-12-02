eb3968aa2b8173204cc50024a63eea04e39dbb96
Peter Seale <peter@pseale.com>
2021-12-02 14:14:07 -0600

    add scaffolding for day 05 part a

---
+++ 2019-csharp/AdventOfCode2019/Day05/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day05;
+
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = File.ReadAllText("input.txt");
+        var partA = SolvePartA(input);
+        Console.WriteLine($"Diagnostic code: {partA}");
+    }
+
+    private static int SolvePartA(string input)
+    {
+        return -1;
+    }
+
+    public static int[] ExecuteIntcode(string input)
+    {
+        return new int[0];
+    }
+}
---
+++ 2019-csharp/AdventOfCode2019/Tests/Day05Tests.cs
@@ -0,0 +1,19 @@
+﻿using Day05;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class Day05Tests
+{
+    [Test]
+    public void PartA()
+    {
+        Assert.AreEqual("1002,4,3,4,99", Csvify(Program.ExecuteIntcode("1002,4,3,4,33")));
+    }
+
+    private string Csvify(int[] memory)
+    {
+        return string.Join(",", memory);
+    }
+}
