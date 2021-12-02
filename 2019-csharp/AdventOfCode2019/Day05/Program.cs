bba4fe5faa54ab557e0011c9c7b7d7a11d112c44
Peter Seale <peter@pseale.com>
2021-12-02 15:10:56 -0600

    WIP: many bothans died

--- 2019-csharp/AdventOfCode2019/Day05/Program.cs
+++ 2019-csharp/AdventOfCode2019/Day05/Program.cs
@@ -16,6 +16,66 @@ public static class Program
 
     public static int[] ExecuteIntcode(string input)
     {
-        return new int[0];
+        var memory = input
+             .Split(",")
+             .Select(x => x.Trim())
+             .Where(x => !string.IsNullOrWhiteSpace(x))
+             .Select(x => int.Parse(x))
+             .ToArray();
+
+        int position = 0;
+
+        ExecuteIntcodeLoop(memory, position);
+
+        return memory;
+    }
+
+    private static void ExecuteIntcodeLoop(int[] memory, int position)
+    {
+        while (true)
+        {
+            var rawOpcode = memory[position];
+            var opcode = rawOpcode % 100;
+            var parameter1Mode = rawOpcode / 100 % 10;
+            var parameter2Mode = rawOpcode / 1000 % 10;
+            var parameter3Mode = rawOpcode / 10000;
+
+            switch (opcode)
+            {
+                case 1:
+                    var add1 = Get(memory, position + 1, parameter1Mode);
+                    var add2 = Get(memory, position + 2, parameter2Mode);
+                    var addOutputPosition = Get(memory, position + 3, parameter3Mode, false);
+                    memory[addOutputPosition] = add1 + add2;
+                    position += 4;
+                    break;
+                case 2:
+                    var multiply1 = Get(memory, position + 1, parameter1Mode);
+                    var multiply2 = Get(memory, position + 2, parameter2Mode);
+                    var multiplyOutputPosition = Get(memory, position + 3, parameter3Mode, false);
+                    memory[multiplyOutputPosition] = multiply1 * multiply2;
+                    position += 4;
+                    break;
+                case 99:
+                    return;
+                default:
+                    throw new NotImplementedException($"Opcode {opcode} at position {position}");
+            }
+        }
+    }
+
+    private static int Get(int[] memory, int position, int parameterMode, bool dereference = true)
+    {
+        var value = dereference ? memory[position] : position;
+
+        // position mode
+        if (parameterMode == 0)
+            return memory[value];
+
+        // immediate mode
+        if (parameterMode == 1)
+            return value;
+
+        throw new NotImplementedException($"Parameter mode {parameterMode} not implemented");
     }
 }
