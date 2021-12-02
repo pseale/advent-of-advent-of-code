f4888644b452e70fcbc97932f74573c1a3665c48
Peter Seale <peter@pseale.com>
2021-12-02 11:05:08 -0600

    solve day 02 part b

--- 2021-csharp/Day02/Program.cs
+++ 2021-csharp/Day02/Program.cs
@@ -7,8 +7,12 @@ public static class Program
     static void Main()
     {
         var input = File.ReadAllText("input.txt");
+
         var partA = SolvePartA(input);
         Console.WriteLine($"Multiplied final horizontal position by final depth: {partA}");
+
+        var partB = SolvePartB(input);
+        Console.WriteLine($"Multiplied final horizontal position by final depth (using new interpretation): {partB}");
     }
 
     public static int SolvePartA(string input)
@@ -42,6 +46,47 @@ public static class Program
                     break;
                 case "down":
                     depth += instruction.Units;
+                    break;
+                default:
+                    throw new Exception($"Invalid submarine instruction: {instruction.Direction}");
+            }
+        }
+        return depth * horizontalPosition;
+    }
+
+    public static int SolvePartB(string input)
+    {
+        var lines = input
+            .Split("\n")
+            .Select(x => x.Trim())
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .ToArray();
+
+        var instructions = new List<SubmarineInstruction>();
+        foreach (var line in lines)
+        {
+            var words = line.Split(" ");
+            instructions.Add(new SubmarineInstruction(words[0], int.Parse(words[1])));
+        }
+
+        int aim = 0;
+        int depth = 0;
+        int horizontalPosition = 0;
+        foreach (var instruction in instructions)
+        {
+            switch (instruction.Direction)
+            {
+                case "forward":
+                    horizontalPosition += instruction.Units;
+                    depth += aim * instruction.Units;
+                    if (depth < 0)
+                        throw new Exception("Depth should never go negative! Submarines can't fly.");
+                    break;
+                case "up":
+                    aim -= instruction.Units;
+                    break;
+                case "down":
+                    aim += instruction.Units;
                     break;
                 default:
                     throw new Exception($"Invalid submarine instruction: {instruction.Direction}");
--- 2021-csharp/Tests/Day02Tests.cs
+++ 2021-csharp/Tests/Day02Tests.cs
@@ -18,4 +18,17 @@ public class Day02Tests
 
         Assert.AreEqual(15*10, Program.SolvePartA(input));
     }
+
+    [Test]
+    public void PartB()
+    {
+        var input = @"forward 5
+                      down 5
+                      forward 8
+                      up 3
+                      down 8
+                      forward 2";
+
+        Assert.AreEqual(15*60, Program.SolvePartB(input));
+    }
 }
