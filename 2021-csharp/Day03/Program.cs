f5a9ac2d1f76e9cde8e30c4b197a61d3858d146f
Peter Seale <peter@pseale.com>
2021-12-03 13:10:03 -0600

    add scaffolding for day 03 part b - don't attempt to compile this (DO NOT)

--- 2021-csharp/Day03/Program.cs
+++ 2021-csharp/Day03/Program.cs
@@ -8,6 +8,9 @@ public static class Program
 
         var partA = SolvePartA(input);
         Console.WriteLine($"Power consumption of the submarine: {partA}");
+
+        var partB = SolvePartB(input);
+        Console.WriteLine($"Life support rating of the submarine: {partB}");
     }
 
     public static int SolvePartA(string input)
@@ -53,5 +56,46 @@ public static class Program
         var epsilonRate = Convert.ToInt32(string.Join("", leastCommonBits), 2);
 
         return gammaRate * epsilonRate;
+    }
+
+    public static int SolvePartB(string input)
+    {
+        var lines = input
+            .Split("\n")
+            .Select(x => x.Trim())
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .ToArray();
+
+        var diagnosticReport = lines
+            .Select(x => x.ToCharArray())
+            .ToArray();
+
+        // apology: I'm aware that oneBits can be inferred from zeroBits, and don't need to
+        // be stored. But, it felt great and I'm honestly not apologizing.
+        var zeroBits = new int[diagnosticReport[0].Length];
+        var oneBits = new int[diagnosticReport[0].Length];
+
+        foreach (var line in diagnosticReport)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '0')
+                    zeroBits[i]++;
+                else
+                    oneBits[i]++;
+            }
+        }
+
+        // apology: see above. I'm aware that leastCommonBits can be inferred from mostCommonBits,
+        // and am choosing to double down on the duplication. 👍👍
+        var mostCommonBits = new char[diagnosticReport[0].Length];
+        var leastCommonBits = new char[diagnosticReport[0].Length];
+        for (int i = 0; i < diagnosticReport[0].Length; i++)
+        {
+            mostCommonBits[i] = oneBits[i] > zeroBits[i] ? '1' : '0';
+            leastCommonBits[i] = oneBits[i] > zeroBits[i] ? '0' : '1';
+        }
+
+        return -1;
     }
 }
--- 2021-csharp/Tests/Day03Tests.cs
+++ 2021-csharp/Tests/Day03Tests.cs
@@ -24,4 +24,23 @@ public class Day03Tests
 
         Assert.AreEqual(22*9, Program.SolvePartA(input));
     }
+
+    [Test]
+    public void PartB()
+    {
+        var input = @"00100
+                      11110
+                      10110
+                      10111
+                      10101
+                      01111
+                      00111
+                      11100
+                      10000
+                      11001
+                      00010
+                      01010";
+
+        Assert.AreEqual(10*23, Program.SolvePartB(input));
+    }
 }
