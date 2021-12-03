a37c9fd3230e8fbdf6947e48e5643a6464bfd593
Peter Seale <peter@pseale.com>
2021-12-03 14:14:21 -0600

    solve day 03 part b - reminder that Advent of Code is The Purge, where all crimes are legal

--- 2021-csharp/Day03/Program.cs
+++ 2021-csharp/Day03/Program.cs
@@ -2,6 +2,7 @@
 
 public static class Program
 {
+    // general apology: There is a LOT (A LOT) of duplicated code here. You have been warned.
     static void Main()
     {
         var input = File.ReadAllText("input.txt");
@@ -70,12 +71,60 @@ public static class Program
             .Select(x => x.ToCharArray())
             .ToArray();
 
+        char[] mostCommonBits;
+        char[] leastCommonBits;
+        (mostCommonBits, _) = Get(diagnosticReport);
+        var oxygenGeneratorRatingBits = new List<char>();
+        var remaining = diagnosticReport;
+        for (int i = 0; i < diagnosticReport[0].Length; i++)
+        {
+            var bit = mostCommonBits[i];
+            oxygenGeneratorRatingBits.Add(bit);
+
+            remaining = remaining.Where(x => x[i] == bit).ToArray();
+            (mostCommonBits, _) = Get(remaining);
+            if (remaining.Length == 1)
+            {
+                oxygenGeneratorRatingBits = remaining[0].ToList();
+                break;
+            }
+        }
+
+        (_, leastCommonBits) = Get(diagnosticReport);
+        var co2ScrubberRatingBits = new List<char>();
+        remaining = diagnosticReport;
+        for (int i = 0; i < diagnosticReport[0].Length; i++)
+        {
+            var bit = leastCommonBits[i];
+            co2ScrubberRatingBits.Add(bit);
+
+            remaining = remaining.Where(x => x[i] == bit).ToArray();
+            if (remaining.Length == 1)
+            {
+                co2ScrubberRatingBits = remaining[0].ToList();
+                break;
+            }
+            (_, leastCommonBits) = Get(remaining);
+        }
+
+        var oxygenGeneratorRating = Convert.ToInt32(string.Join("", oxygenGeneratorRatingBits), 2);
+        var co2ScrubberRating = Convert.ToInt32(string.Join("", co2ScrubberRatingBits), 2);
+
+        return oxygenGeneratorRating * co2ScrubberRating;
+    }
+
+    private static (char[], char[]) Get(char[][] lines)
+    {
+        var length = lines[0].Length;
+
+        var mostCommonBits = new char[length];
+        var leastCommonBits = new char[length];
         // apology: I'm aware that oneBits can be inferred from zeroBits, and don't need to
         // be stored. But, it felt great and I'm honestly not apologizing.
-        var zeroBits = new int[diagnosticReport[0].Length];
-        var oneBits = new int[diagnosticReport[0].Length];
+        var zeroBits = new int[length];
+        var oneBits = new int[length];
 
-        foreach (var line in diagnosticReport)
+        foreach (var line in lines)
         {
             for (int i = 0; i < line.Length; i++)
             {
@@ -88,14 +137,12 @@ public static class Program
 
         // apology: see above. I'm aware that leastCommonBits can be inferred from mostCommonBits,
         // and am choosing to double down on the duplication. 👍👍
-        var mostCommonBits = new char[diagnosticReport[0].Length];
-        var leastCommonBits = new char[diagnosticReport[0].Length];
-        for (int i = 0; i < diagnosticReport[0].Length; i++)
+        for (int i = 0; i < length; i++)
         {
-            mostCommonBits[i] = oneBits[i] > zeroBits[i] ? '1' : '0';
-            leastCommonBits[i] = oneBits[i] > zeroBits[i] ? '0' : '1';
+            mostCommonBits[i] = oneBits[i] >= zeroBits[i] ? '1' : '0';
+            leastCommonBits[i] = zeroBits[i] <= oneBits[i] ? '0' : '1';
         }
 
-        return -1;
+        return (mostCommonBits, leastCommonBits);
     }
 }
