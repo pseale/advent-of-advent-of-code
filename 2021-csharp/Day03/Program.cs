a8fd5641e64b7bd06a8e25577bd9ecba84b92cd0
Peter Seale <peter@pseale.com>
2021-12-03 12:34:13 -0600

    add scaffolding for day 03 part a

---
+++ 2021-csharp/Day03/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day03;
+
+public static class Program
+{
+    static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        var partA = SolvePartA(input);
+        Console.WriteLine($"Power consumption of the submarine: {partA}");
+    }
+
+    public static int SolvePartA(string input)
+    {
+        return -1;
+    }
+}
---
+++ 2021-csharp/Tests/Day03Tests.cs
@@ -0,0 +1,27 @@
+﻿using Day03;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class Day03Tests
+{
+    [Test]
+    public void PartA()
+    {
+        var input = @"00100
+                      11110
+                      10110
+                      10111
+                      10101
+                      01111
+                      00111
+                      11100
+                      10000
+                      11001
+                      00010
+                      01010";
+
+        Assert.AreEqual(22*9, Program.SolvePartA(input));
+    }
+}
