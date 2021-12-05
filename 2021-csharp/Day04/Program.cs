8b40d3d943b444fdfe1165795a3015346872af77
Peter Seale <peter@pseale.com>
2021-12-05 15:54:41 -0600

    add scaffolding for day 04 part a

---
+++ 2021-csharp/Day04/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day04;
+
+public static class Program
+{
+    static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        var partA = SolvePartA(input);
+        Console.WriteLine($"Final score: {partA}");
+    }
+
+    public static int SolvePartA(string input)
+    {
+        return -1;
+    }
+}
+
