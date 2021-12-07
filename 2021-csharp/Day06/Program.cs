b5c121af68e9342f0bca2b9f17fb50e591f80db8
Peter Seale <peter@pseale.com>
2021-12-06 21:50:05 -0600

    solve day 06 part a

--- 2021-csharp/Day06/Program.cs
+++ 2021-csharp/Day06/Program.cs
@@ -4,15 +4,42 @@ public static class Program
 {
     private static void Main()
     {
-        var input = File.ReadAllText("input.txt");
+        string input = File.ReadAllText("input.txt");
 
-        var partA = SolvePartA(input);
+        int partA = SolvePartA(input);
         // ReSharper disable once StringLiteralTypo
         Console.WriteLine($"Lanternfish after 80 days: {partA}");
     }
 
     public static int SolvePartA(string input)
     {
-        return -1;
+        // ReSharper disable once ReplaceWithSingleCallToSingle
+        string line = input
+            .Split("\n")
+            .Select(x => x.Trim())
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .Single();
+
+        List<int> fish = line.Split(",")
+            .Select(x => int.Parse(x))
+            .ToList();
+
+        for (int day = 0; day < 80; day++)
+        {
+            List<int> newFish = new List<int>();
+            for (int i = 0; i < fish.Count; i++)
+            {
+                fish[i]--;
+                if (fish[i] < 0)
+                {
+                    newFish.Add(8);
+                    fish[i] = 6;
+                }
+            }
+
+            fish.AddRange(newFish);
+        }
+
+        return fish.Count;
     }
 }
