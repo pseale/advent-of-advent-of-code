af90abbebad10e81875807878e2dcc7df2e82a41
Peter Seale <peter@pseale.com>
2021-12-06 19:43:18 -0600

    add scaffolding for day 05 part a

---
+++ 2021-csharp/Day05/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day05;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        var partA = SolvePartA(input);
+        Console.WriteLine($"Points where at least two lines overlap: {partA}");
+    }
+
+    public static int SolvePartA(string input)
+    {
+        return -1;
+    }
+}
---
+++ 2021-csharp/Tests/Day05Tests.cs
@@ -0,0 +1,25 @@
+﻿using Day05;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class Day05Tests
+{
+    [Test]
+    public void PartA()
+    {
+        var input = @"0,9 -> 5,9
+                      8,0 -> 0,8
+                      9,4 -> 3,4
+                      2,2 -> 2,1
+                      7,0 -> 7,4
+                      6,4 -> 2,0
+                      0,9 -> 2,9
+                      3,4 -> 1,4
+                      0,0 -> 8,8
+                      5,5 -> 8,2";
+
+        Assert.AreEqual(5, Program.SolvePartA(input));
+    }
+}
