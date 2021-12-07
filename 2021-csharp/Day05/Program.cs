50fcbac28706e17bc86c9c66318a54620d0b76d3
Peter Seale <peter@pseale.com>
2021-12-06 20:55:08 -0600

    solve day 05 part a

--- 2021-csharp/Day05/Program.cs
+++ 2021-csharp/Day05/Program.cs
@@ -61,6 +61,7 @@ public static class Program
         return hotspots;
     }
 
+    // apology: I am ashamed of this. It works though 👍
     private static List<Point> PartAGetPoints(Vent vent)
     {
         var leftRightDirection = vent.Col2 - vent.Col1 > 0
@@ -86,7 +87,71 @@ public static class Program
 
     public static int SolvePartB(string input)
     {
-        return -1;
+        var l = input
+            .Split("\n")
+            .Select(x => x.Trim())
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .ToArray();
+
+        var vents = new List<Vent>();
+        foreach (var line in l)
+        {
+            var words = line.Split(" ");
+            // 0,9 -> 5,9
+            var col1 = int.Parse(words[0].Split(",")[0]);
+            var row1 = int.Parse(words[0].Split(",")[1]);
+
+            var col2 = int.Parse(words[2].Split(",")[0]);
+            var row2 = int.Parse(words[2].Split(",")[1]);
+            vents.Add(new Vent(col1, row1, col2, row2));
+        }
+
+
+        var maxCol = vents.Max(x => Math.Max(x.Col1, x.Col2)) + 2;
+        var maxRow = vents.Max(x => Math.Max(x.Row1, x.Row2)) + 2;
+
+        var points = new int[maxCol, maxRow];
+
+        foreach (var vent in vents)
+        {
+            foreach (var point in PartBGetPoints(vent))
+                points[point.Col, point.Row] += 1;
+        }
+
+        var hotspots = 0;
+        for (int col = 0; col < maxCol; col++)
+            for (int row = 0; row < maxRow; row++)
+                if (points[col, row] >= 2)
+                    hotspots++;
+
+        return hotspots;
+    }
+
+    // retraction: no longer ashamed of the apology comment above. Live out loud 👍👍
+    private static List<Point> PartBGetPoints(Vent vent)
+    {
+        var leftRightDistance = (vent.Col2 - vent.Col1);
+        var upDownDistance = vent.Row2 - vent.Row1;
+
+        var leftRightDirection = leftRightDistance > 0
+            ? 1
+            : leftRightDistance < 0 ? -1 : 0;
+
+        var upDownDirection = upDownDistance > 0
+            ? 1
+            : upDownDistance < 0 ? -1 : 0;
+
+        var distance = Math.Max(Math.Abs(leftRightDistance), Math.Abs(upDownDistance));
+
+        // 9,4 -> 3,4      9,4 8,4 7,4 6,4 5,4 4,4 3,4
+        //                 i=0 i=1 i=2 i=3 i=4 i=5 i=6
+        var points = new List<Point>();
+        for (int i = 0; i <= distance; i++)
+        {
+            points.Add(new Point(vent.Col1 + i * leftRightDirection, vent.Row1 + i * upDownDirection));
+        }
+
+        return points;
     }
 
     public record Vent(int Col1, int Row1, int Col2, int Row2);
