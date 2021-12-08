13637d81bacb8b8366cd5a85083811909c95d4ab
Peter Seale <peter@pseale.com>
2021-12-07 19:28:39 -0600

    solve day 07 part a

--- 2021-csharp/Day07/Program.cs
+++ 2021-csharp/Day07/Program.cs
@@ -13,6 +13,31 @@ public static class Program
 
     public static int SolvePartA(string input)
     {
-        return -1;
+        // ReSharper disable once ReplaceWithSingleCallToSingle
+        var line = input
+            .Split("\n")
+            .Select(x => x.Trim())
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .Single();
+
+        var crabSubmarines = line.Split(",")
+            .Select(x => int.Parse(x))
+            .ToList();
+        var horizontalPositions = crabSubmarines.Max() + 1;
+
+        var fuelCosts = new int[horizontalPositions];
+        for (int i = 0; i < horizontalPositions; i++)
+        {
+            fuelCosts[i] = CalculateFuelCosts(crabSubmarines, i);
+        }
+
+        return fuelCosts.Min();
+    }
+
+    private static int CalculateFuelCosts(List<int> crabSubmarines, int horizontalPosition)
+    {
+        return crabSubmarines
+            .Select(submarine => Math.Abs(submarine - horizontalPosition))
+            .Sum();
     }
 }
