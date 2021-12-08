732073033a993ac3c6a450ba57cc4b80f16d63b3
Peter Seale <peter@pseale.com>
2021-12-08 16:45:01 -0600

    add scaffolding for day 08 part a

---
+++ 2021-csharp/Day08/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day08;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        var partA = SolvePartA(input);
+        Console.WriteLine($"digits 1,4,7,8 appear this many times in the outputValue: {partA}");
+    }
+
+    public static int SolvePartA(string input)
+    {
+        return -1;
+    }
+}
