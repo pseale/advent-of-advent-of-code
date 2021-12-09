c0898d279976724c116a08af7533b68bf274ea6a
Peter Seale <peter@pseale.com>
2021-12-09 12:51:36 -0600

    add scaffolding for day 09 part a

---
+++ 2021-csharp/Day09/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day09;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        var partA = SolvePartA(input);
+        Console.WriteLine($"Sum of the risk levels of all low points: {partA}");
+    }
+
+    public static int SolvePartA(string input)
+    {
+        return -1;
+    }
+}
---
+++ 2021-csharp/Tests/Day09Tests.cs
@@ -0,0 +1,20 @@
+﻿using Day09;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class Day09Tests
+{
+    [Test]
+    public void PartA()
+    {
+        var input = @"2199943210
+                      3987894921
+                      9856789892
+                      8767896789
+                      9899965678";
+
+        Assert.AreEqual(2 + 1 + 6 + 6, Program.SolvePartA(input));
+    }
+}
