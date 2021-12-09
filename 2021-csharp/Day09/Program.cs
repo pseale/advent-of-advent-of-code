db4e359a26ad78c10217a0d102313bef2294e7dd
Peter Seale <peter@pseale.com>
2021-12-09 13:11:02 -0600

    solve day 09 part a

--- 2021-csharp/Day09/Program.cs
+++ 2021-csharp/Day09/Program.cs
@@ -2,6 +2,9 @@
 
 public static class Program
 {
+    private static int _rows;
+    private static int _cols;
+
     private static void Main()
     {
         var input = File.ReadAllText("input.txt");
@@ -12,6 +15,56 @@ public static class Program
 
     public static int SolvePartA(string input)
     {
-        return -1;
+        var lines = input
+            .Split("\n")
+            .Select(x => x.Trim())
+            .Where(x => !string.IsNullOrWhiteSpace(x))
+            .ToArray();
+
+        _rows = lines.Length;
+        _cols = lines[0].Length;
+
+        var grid = new int[_cols, _rows];
+        for (int row = 0; row < _rows; row++)
+        {
+            var numbers = lines[row]
+                .ToCharArray()
+                .Select(x => int.Parse(x.ToString()))
+                .ToArray();
+            for (int col = 0; col < _cols; col++)
+            {
+                grid[col, row] = numbers[col];
+            }
+        }
+
+        var totalRiskLevel = 0;
+        for (int row = 0; row < _rows; row++)
+        for (int col = 0; col < _cols; col++)
+        {
+            if (IsLowPoint(grid, col, row))
+                totalRiskLevel += 1 + grid[col, row];
+        }
+        return totalRiskLevel;
+    }
+
+    private static bool IsLowPoint(int[,] grid, int col, int row)
+    {
+        var height = grid[col, row];
+
+        return IsHigher(grid, col + 1, row, height)
+               && IsHigher(grid, col - 1, row, height)
+               && IsHigher(grid, col, row + 1, height)
+               && IsHigher(grid, col, row - 1, height);
+    }
+
+    // Returns true if the point is higher, or returns true if the point doesn't exist on the grid
+    private static bool IsHigher(int[,] grid, int col, int row, int height)
+    {
+        if (col < 0 || col >= _cols)
+            return true;
+        if (row < 0 || row >= _rows)
+            return true;
+
+        return grid[col, row] > height;
     }
 }
