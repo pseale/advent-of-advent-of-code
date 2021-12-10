1fc2a2f2475f6a94e577616b52f1a3ac89bcfca4
Peter Seale <peter@pseale.com>
2021-12-10 12:47:01 -0600

    solve day 10 part a

--- 2021-csharp/Day10/Program.cs
+++ 2021-csharp/Day10/Program.cs
@@ -12,6 +12,56 @@ public static class Program
 
     public static int SolvePartA(string input)
     {
-        return -1;
+        var scores = new List<int>();
+
+        var queue = new Queue<char>(input.ToCharArray());
+        var stack = new Stack<char>();
+        while (queue.Any())
+        {
+            var @char = queue.Dequeue();
+            if (@char is ' ' or '\n' or '\r')
+            {
+                stack.Clear();
+                continue;
+            }
+
+            if (@char is '[' or '<' or '{' or '(')
+            {
+                stack.Push(@char);
+            }
+            else if (@char is ']' or '>' or '}' or ')')
+            {
+               if (stack.Any() && LegallyCloses(stack.Peek(), @char))
+               {
+                   stack.Pop();
+               }
+               else
+               {
+                   scores.Add(Score(@char));
+                   while (queue.Any() && queue.Peek() is not '\r' or '\n') queue.Dequeue();
+               }
+            }
+        }
+        return scores.Sum();
+    }
+
+    private static bool LegallyCloses(char opening, char closing)
+    {
+        if (opening == '(') return closing == ')';
+        if (opening == '[') return closing == ']';
+        if (opening == '<') return closing == '>';
+        if (opening == '{') return closing == '}';
+
+        throw new Exception($"Invalid opening character: {opening}");
+    }
+
+    private static int Score(char @char)
+    {
+        if (@char == ')') return 3;
+        if (@char == ']') return 57;
+        if (@char == '}') return 1197;
+        if (@char == '>') return 25137;
+
+        throw new Exception($"Invalid closing character: {@char}");
     }
 }
