c2b4175216662b41a3c2a74f6595851003c88d4b
Peter Seale <peter@pseale.com>
2021-12-10 13:25:23 -0600

    solve day 10 part b

--- 2021-csharp/Day10/Program.cs
+++ 2021-csharp/Day10/Program.cs
@@ -48,9 +48,71 @@ public static class Program
         return scores.Sum();
     }
 
-    public static int SolvePartB(string input)
+    public static long SolvePartB(string input)
     {
-        throw new NotImplementedException();
+        var scores = new List<long>();
+
+        var queue = new Queue<char>(input.ToCharArray());
+        var stack = new Stack<char>();
+        while (queue.Any())
+        {
+            var @char = queue.Dequeue();
+            if (@char is ' ' or '\n' or '\r')
+            {
+                var score = EmptyTheStackAndGetAutocompleteScore(stack);
+                if (score != 0) scores.Add(score);
+                continue;
+            }
+
+            if (@char is '[' or '<' or '{' or '(')
+            {
+                stack.Push(@char);
+            }
+            else if (@char is ']' or '>' or '}' or ')')
+            {
+               if (stack.Any() && LegallyCloses(stack.Peek(), @char))
+               {
+                   stack.Pop();
+               }
+               else
+               {
+                   stack.Clear();
+                   while (queue.Any() && queue.Peek() is not '\r' or '\n') queue.Dequeue();
+               }
+            }
+        }
+
+        // count final line?
+        if (stack.Any())
+            scores.Add(EmptyTheStackAndGetAutocompleteScore(stack));
+        if (scores.Count % 2 == 0) throw new Exception($"Expected odd # of incomplete lines, got {scores.Count()}");
+
+        scores.Sort(); // apology: no one should ever use this method
+        return scores[scores.Count / 2];
+    }
+
+    private static long EmptyTheStackAndGetAutocompleteScore(Stack<char> stack)
+    {
+        long score = 0;
+
+        while (stack.Any())
+        {
+            var @char = stack.Pop();
+            var charScore = GetAutocompleteScoreForChar(@char);
+            score = score * 5 + charScore;
+        }
+
+        return score;
+    }
+
+    private static int GetAutocompleteScoreForChar(char @char)
+    {
+        if (@char == '(') return 1;
+        if (@char == '[') return 2;
+        if (@char == '{') return 3;
+        if (@char == '<') return 4;
+
+        throw new Exception($"Invalid char: {@char}");
     }
 
     private static bool LegallyCloses(char opening, char closing)
