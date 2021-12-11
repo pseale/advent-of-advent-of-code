bd35994073b1e67c4f90c539a0a40a8767dc4abb
Peter Seale <peter@pseale.com>
2021-12-11 13:22:24 -0600

    add scaffolding for day 11 part a

---
+++ 2021-csharp/Day11/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day11;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        var partA = SolvePartA(input);
+        Console.WriteLine($"Total flashes after 100 steps: {partA}");
+    }
+
+    public static int SolvePartA(string input)
+    {
+        return -1;
+    }
+}
---
+++ 2021-csharp/Tests/Day11Tests.cs
@@ -0,0 +1,25 @@
+﻿using Day11;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class Day11Tests
+{
+    [Test]
+    public void PartA()
+    {
+        var input = @"5483143223
+                      2745854711
+                      5264556173
+                      6141336146
+                      6357385478
+                      4167524645
+                      2176841721
+                      6882881134
+                      4846848554
+                      5283751526";
+
+        Assert.AreEqual(1656, Program.SolvePartA(input));
+    }
+}
