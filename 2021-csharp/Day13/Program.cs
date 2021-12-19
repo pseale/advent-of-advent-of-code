fd0292f11949fde686667572ee203c854e712351
Peter Seale <peter@pseale.com>
2021-12-19 16:51:19 -0600

    add scaffolding for day 13 part a

---
+++ 2021-csharp/Day13/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Day13;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        var partA = SolvePartA(input);
+        Console.WriteLine($"Visible dots after first fold: {partA}");
+    }
+
+    public static int SolvePartA(string input)
+    {
+        return -1;
+    }
+}
