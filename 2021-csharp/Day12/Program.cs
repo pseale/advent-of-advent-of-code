1cc76c0a6adddaa8928cf98579a6462d1d0fd2aa
Peter Seale <peter@pseale.com>
2021-12-19 15:08:01 -0600

    add scaffolding for day 12 part a

---
+++ 2021-csharp/Day12/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Day12;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        var partA = SolvePartA(input);
+        Console.WriteLine($"Paths: {partA}");
+    }
+
+    public static int SolvePartA(string input)
+    {
+        return -1;
+    }
+}
---
+++ 2021-csharp/Tests/Day12Tests.cs
@@ -0,0 +1,58 @@
+﻿using Day12;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class Day12Tests
+{
+    [Test]
+    public void PartAShorterTestCase()
+    {
+        var input = @"start-A
+                      start-b
+                      A-c
+                      A-b
+                      b-d
+                      A-end
+                      b-end";
+
+        Assert.AreEqual(10, Program.SolvePartA(input));
+    }
+
+    [Test]
+    public void PartALargerTestCases()
+    {
+        var input = @"dc-end
+                      HN-start
+                      start-kj
+                      dc-start
+                      dc-HN
+                      LN-dc
+                      HN-end
+                      kj-sa
+                      kj-HN
+                      kj-dc";
+        Assert.AreEqual(19, Program.SolvePartA(input));
+
+        var evenLargerInput = @"fs-end
+                                he-DX
+                                fs-he
+                                start-DX
+                                pj-DX
+                                end-zg
+                                zg-sl
+                                zg-pj
+                                pj-he
+                                RW-he
+                                fs-DX
+                                pj-RW
+                                zg-RW
+                                start-pj
+                                he-WI
+                                zg-he
+                                pj-fs
+                                start-RW";
+        Assert.AreEqual(226, Program.SolvePartA(evenLargerInput));
+    }
+}
