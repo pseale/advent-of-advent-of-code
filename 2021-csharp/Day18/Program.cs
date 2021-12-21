b48c1f0ad87ed45429de609b0fb48e3b75a7e83a
Peter Seale <peter@pseale.com>
2021-12-20 19:49:44 -0600

    add walking skeleton for day 18 part a

---
+++ 2021-csharp/Day18/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Day18;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        var partA = SolvePartA(input);
+        Console.WriteLine($"Magnitude of the final sum: {partA}");
+    }
+
+    public static int SolvePartA(string input)
+    {
+        return -1;
+    }
+}
