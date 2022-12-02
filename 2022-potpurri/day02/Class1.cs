b91b062a408ee58c72dd7d7dcdeff4e391e0ecaa
Peter Seale <peter@pseale.com>
2022-12-01 23:26:02 -0600

    solve day 02 - raw

---
+++ 2022-potpurri/day02/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02
+{
+public enum RPS
+{
+    Rock = 1,
+    Paper =2,
+    Scissors =3
+};
+    public class Round
+    {
+        public RPS Them;
+        public RPS Us;
+    }
+}
---
+++ 2022-potpurri/day02/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+using Day02;
+
+var input = await File.ReadAllLinesAsync(@".\input.txt");
+
+RPS Parse(string v)
+{
+    if (v == "A" || v == "X")
+        return RPS.Rock;
+    if (v == "B" || v == "Y")
+        return RPS.Paper;
+    if (v == "C" || v == "Z")
+        return RPS.Scissors;
+
+    throw new NotImplementedException();
+}
+
+long SolvePartA(string[] input)
+{
+    var rounds = input.Where(x => !string.IsNullOrWhiteSpace(x))
+        .Select(x => new Round { Them = Parse(x.Split(" ")[0]), Us = Parse(x.Split(" ")[1]) });
+    long score = 0;
+    foreach (var round in rounds)
+    {
+        score += (long)round.Us;
+        if (round.Us == round.Them)
+        {
+            score += 3;
+        }
+        else if (round.Us == RPS.Rock && round.Them == RPS.Scissors
+            || round.Us == RPS.Paper && round.Them == RPS.Rock
+            || round.Us == RPS.Scissors && round.Them == RPS.Paper)
+        {
+            score += 6;
+        }
+        else
+        {
+            score += 0;
+        }
+    }
+
+    return score;
+}
+
+var exampleInput = new[] { "A Y", "B X", "C Z" };
+var examplePartA = SolvePartA(exampleInput);
+
+
+Console.WriteLine($"EXAMPLE Part A: {examplePartA} (expected 15)");
+
+var partA = SolvePartA(input);
+var partB = "";
+Console.WriteLine($"Part A: {partA}");
+
+Console.WriteLine($"Part B: {partB}");
