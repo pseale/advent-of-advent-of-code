a20bcc88ce32c3f83d6e7ebd56bf1145713409f7
Peter Seale <peter@pseale.com>
2022-12-02 23:36:10 -0600

    refactor: cleanup

--- 2022-potpurri/day03/Program.cs
+++ 2022-potpurri/day03/Program.cs
@@ -1,66 +1,5 @@
 ﻿var exampleInput = await File.ReadAllLinesAsync(@".\example.txt");
-
-static long Score(char c)
-{
-    if (c >= 'a' && c <= 'z') return 0 + 1 + ((long)c - (long)'a');
-    else if (c >= 'A' && c <= 'Z') return 26 + 1 + ((long)c - (long)'A');
-    throw new NotImplementedException();
-}
-
 var input = await File.ReadAllLinesAsync(@".\input.txt");
-
-long SolvePartA(string[] input)
-{
-    long score = 0;
-    foreach (var line in input)
-    {
-        var leftHalf = line.Substring(0, line.Length / 2);
-        var rightHalf = line.Substring(line.Length / 2);
-        char c = FindSameCharIn(leftHalf, rightHalf);
-
-        score += Score(c);
-    }
-    return score;
-}
-
-char FindSameCharIn(string leftHalf, string rightHalf)
-{
-    var dictionary = new Dictionary<char, int>();
-
-    leftHalf.ToCharArray().Distinct().ToList().ForEach(x => {
-        if (!dictionary.TryAdd(x, 1)) dictionary[x] += 1;
-    });
-    rightHalf.ToCharArray().Distinct().ToList().ForEach(x => {
-        if (!dictionary.TryAdd(x, 1)) dictionary[x] += 1;
-    });
-    return dictionary.Single(x => x.Value == 2).Key;
-}
-
-long SolvePartB(string[] input)
-{
-    long score = 0;
-    var batches = input.Batch(3).ToArray();
-    foreach (var batch in batches)
-    {
-        char c = FindSameCharInBatch(batch);
-        score += Score(c);
-    }
-    return score;
-}
-
-char FindSameCharInBatch(IEnumerable<string> batch)
-{
-    var dictionary = new Dictionary<char, int>();
-
-    foreach (var b in batch)
-    {
-        b.ToCharArray().Distinct().ToList().ForEach(x => {
-            if (!dictionary.TryAdd(x, 1)) dictionary[x] += 1;
-        });
-    }
-    return dictionary.Single(x => x.Value == 3).Key;
-}
-
 
 var examplePartA = SolvePartA(exampleInput);
 var partA = SolvePartA(input);
@@ -73,13 +12,47 @@ var partB = SolvePartB(input);
 Console.WriteLine($"EXAMPLE Part B: {examplePartB} (expected 70)");
 Console.WriteLine($"Part B: {partB}");
 
-public static class MyExtensions
+static long Score(char c)
 {
-    public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> items,
-                                                       int maxItems)
+    if (c >= 'a' && c <= 'z') return 0 + 1 + ((long)c - (long)'a');
+    else if (c >= 'A' && c <= 'Z') return 26 + 1 + ((long)c - (long)'A');
+    throw new Exception("Expected 'a-z' or 'A-Z'; got " + c);
+}
+
+long SolvePartA(string[] input)
+{
+    long score = 0;
+    foreach (var line in input)
     {
-        return items.Select((item, inx) => new { item, inx })
-                    .GroupBy(x => x.inx / maxItems)
-                    .Select(g => g.Select(x => x.item));
+        var leftHalf = line.Substring(0, line.Length / 2);
+        var rightHalf = line.Substring(line.Length / 2);
+        char c = FindSameCharInAll(new[] { leftHalf, rightHalf });
+        score += Score(c);
     }
+    return score;
+}
+
+long SolvePartB(string[] input)
+{
+    long score = 0;
+    var batches = input.Chunk(3).ToArray();
+    foreach (var batch in batches)
+    {
+        char c = FindSameCharInAll(batch);
+        score += Score(c);
+    }
+    return score;
+}
+
+char FindSameCharInAll(string[] batch)
+{
+    var dictionary = new Dictionary<char, int>();
+
+    foreach (var b in batch)
+    {
+        b.ToCharArray().Distinct().ToList().ForEach(x => {
+            if (!dictionary.TryAdd(x, 1)) dictionary[x] += 1;
+        });
+    }
+    return dictionary.Single(x => x.Value == batch.Length).Key;
 }
