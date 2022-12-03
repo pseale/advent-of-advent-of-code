7569f7a79773ad61522d18f61588d978db41043c
Peter Seale <peter@pseale.com>
2022-12-02 21:46:43 -0600

    add day03 skeleton

---
+++ 2022-potpurri/day03/Program.cs
@@ -0,0 +1,23 @@
+﻿var exampleInput = await File.ReadAllLinesAsync(@".\example.txt");
+var input = await File.ReadAllLinesAsync(@".\input.txt");
+
+long SolvePartA(string[] input)
+{
+    throw new NotImplementedException();
+}
+long SolvePartB(string[] input)
+{
+    throw new NotImplementedException();
+}
+
+
+var examplePartA = SolvePartA(exampleInput);
+var partA = SolvePartA(input);
+var examplePartB = SolvePartB(exampleInput);
+var partB = SolvePartB(input);
+
+Console.WriteLine($"EXAMPLE Part A: {examplePartA} (expected TODO)");
+Console.WriteLine($"Part A: {partA}");
+Console.WriteLine($"EXAMPLE Part B: {examplePartB} (expected TODO)");
+Console.WriteLine($"Part B: {partB}");
+
