7133d5638d9d3b7c92d240c17b43d5ee69c5bf7e
Peter Seale <peter@pseale.com>
2022-12-02 23:29:55 -0600

    solve day 03 part A - messy

--- 2022-potpurri/day03/Program.cs
+++ 2022-potpurri/day03/Program.cs
@@ -1,23 +1,55 @@
 ﻿var exampleInput = await File.ReadAllLinesAsync(@".\example.txt");
+
+static long Score(char c)
+{
+    if (c >= 'a' && c <= 'z') return 0 + 1 + ((long)c - (long)'a');
+    else if (c >= 'A' && c <= 'Z') return 26 + 1 + ((long)c - (long)'A');
+    throw new NotImplementedException();
+}
+
 var input = await File.ReadAllLinesAsync(@".\input.txt");
 
 long SolvePartA(string[] input)
 {
-    throw new NotImplementedException();
+    long score = 0;
+    foreach (var line in input)
+    {
+        var leftHalf = line.Substring(0, line.Length / 2);
+        var rightHalf = line.Substring(line.Length / 2);
+        char c = FindSameCharIn(leftHalf, rightHalf);
+
+        score += Score(c);
+    }
+    return score;
 }
+
+char FindSameCharIn(string leftHalf, string rightHalf)
+{
+    var dictionary = new Dictionary<char, int>();
+
+    leftHalf.ToCharArray().Distinct().ToList().ForEach(x => {
+        if (!dictionary.TryAdd(x, 1)) dictionary[x] += 1;
+    });
+    rightHalf.ToCharArray().Distinct().ToList().ForEach(x => {
+        if (!dictionary.TryAdd(x, 1)) dictionary[x] += 1;
+    });
+    return dictionary.Single(x => x.Value == 2).Key;
+}
+
 long SolvePartB(string[] input)
 {
-    throw new NotImplementedException();
+    return 0;
 }
 
 
 var examplePartA = SolvePartA(exampleInput);
 var partA = SolvePartA(input);
+
+Console.WriteLine($"EXAMPLE Part A: {examplePartA} (expected 157)");
+Console.WriteLine($"Part A: {partA}");
+
 var examplePartB = SolvePartB(exampleInput);
 var partB = SolvePartB(input);
-
-Console.WriteLine($"EXAMPLE Part A: {examplePartA} (expected TODO)");
-Console.WriteLine($"Part A: {partA}");
 Console.WriteLine($"EXAMPLE Part B: {examplePartB} (expected TODO)");
 Console.WriteLine($"Part B: {partB}");
 
